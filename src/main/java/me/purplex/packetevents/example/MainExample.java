29ec5f6638ad75ee6d70bee2707a02b2c31a9322
purplexdev <uchizi4000@gmail.com>
2020-05-25 19:49:50 +0200

    MainExample (main class) better documented!

--- src/main/java/me/purplex/packetevents/example/MainExample.java
+++ src/main/java/me/purplex/packetevents/example/MainExample.java
@@ -1,21 +1,36 @@
 package me.purplex.packetevents.example;
 
 import me.purplex.packetevents.PacketEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainExample extends JavaPlugin {
     @Override
     public void onEnable() {
         /**
+         * Should the ServerTickEvent ever get called?
+         */
+        final boolean serverTickEventEnabled = true;
+
+        /**
          * Starts the servertickevent repeating task (if enabled)
          * and registers onJoin and onQuit events to inject and uninject players.
-         *
-         * First argument is the JavaPlugin instance to register these events and start the task with,
-         * Second argument enables server tick event
+         * The first argument is an instance to your Main Class,
+         * The second argument is a boolean whether the servertickevent should ever get called
          */
-        PacketEvents.setup(this, true);
+        PacketEvents.setup(this, serverTickEventEnabled);
 
+
+        /**
+         * Register TestExample's PacketListener
+         */
         PacketEvents.getPacketManager().registerListener(new TestExample());
+
+
+        /**
+         * Register TestExample's bukkit Listener
+         */
+        Bukkit.getPluginManager().registerEvents(new TestExample(), this);
     }
 
     @Override
