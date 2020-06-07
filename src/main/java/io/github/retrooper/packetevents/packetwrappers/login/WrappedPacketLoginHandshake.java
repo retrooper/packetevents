51e5cf5c55ac209f280407f859b906b0433282a5
purplexdev <uchizi4000@gmail.com>
2020-06-07 19:46:55 +0200

    Random nms import removed

--- src/main/java/io/github/retrooper/packetevents/packetwrappers/login/WrappedPacketLoginHandshake.java
+++ src/main/java/io/github/retrooper/packetevents/packetwrappers/login/WrappedPacketLoginHandshake.java
@@ -1,13 +1,10 @@
 package io.github.retrooper.packetevents.packetwrappers.login;
 
 import io.github.retrooper.packetevents.packetwrappers.api.WrappedPacket;
-import net.minecraft.server.v1_15_R1.PacketHandshakingInSetProtocol;
-
 public class WrappedPacketLoginHandshake extends WrappedPacket {
     private int protocolVersion, port;
     private String hostname;
 
-    PacketHandshakingInSetProtocol p;
     public WrappedPacketLoginHandshake(final Object packet) {
         super(packet);
     }
