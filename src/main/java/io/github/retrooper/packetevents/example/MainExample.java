ca43582e4fb8a947b58fc0ad6cf2f2637adc8de2
purplexdev <uchizi4000@gmail.com>
2020-06-26 15:56:49 +0200

    1.3.7 Release * Most clientbound wrappers tested, it is assumed the rest will work * 1.16.1 Support * All bugs from 1.3.6.2 fixed * pom.xml updated version * Wrote custom Reflection utils, much cleaner code now and auto caching

--- src/main/java/io/github/retrooper/packetevents/example/MainExample.java
+++ src/main/java/io/github/retrooper/packetevents/example/MainExample.java
@@ -1,19 +1,9 @@
 package io.github.retrooper.packetevents.example;
 
 import io.github.retrooper.packetevents.PacketEvents;
-import io.github.retrooper.packetevents.annotations.PacketHandler;
-import io.github.retrooper.packetevents.event.PacketListener;
-import io.github.retrooper.packetevents.event.impl.PacketReceiveEvent;
-import io.github.retrooper.packetevents.packet.PacketType;
-import io.github.retrooper.packetevents.packetwrappers.in.blockdig.WrappedPacketInBlockDig;
-import io.github.retrooper.packetevents.packetwrappers.in.blockplace.WrappedPacketInBlockPlace;
-import io.github.retrooper.packetevents.packetwrappers.in.chat.WrappedPacketInChat;
-import io.github.retrooper.packetevents.packetwrappers.in.flying.WrappedPacketInFlying;
-import io.github.retrooper.packetevents.packetwrappers.in.keepalive.WrappedPacketInKeepAlive;
-import io.github.retrooper.packetevents.packetwrappers.in.useentity.WrappedPacketInUseEntity;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainExample extends JavaPlugin implements PacketListener {
+public class MainExample extends JavaPlugin {
 
     @Override
     public void onEnable() {
@@ -21,21 +11,11 @@ public class MainExample extends JavaPlugin implements PacketListener {
 
         PacketEvents.getServerTickTask().cancel();
 
-        PacketEvents.getEventManager().registerListener(this);
-
     }
 
     @Override
     public void onDisable() {
         PacketEvents.stop();
-    }
-
-    @PacketHandler
-    public void onReceive(PacketReceiveEvent e) {
-        if(e.getPacketName().equals(PacketType.Client.POSITION)) {
-            WrappedPacketInFlying.WrappedPacketInPosition pos = new WrappedPacketInFlying.WrappedPacketInPosition(e.getNMSPacket());
-            System.out.println("ispos: " + pos.isPosition());
-        }
     }
 
 }
