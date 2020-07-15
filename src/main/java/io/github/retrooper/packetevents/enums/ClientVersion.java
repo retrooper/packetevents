2e6d2286b7193e196192ccddb9074518612074b5
terrarier2111 <58695553+terrarier2111@users.noreply.github.com>
2020-07-15 17:04:37 +0200

    Added Version Lookup

--- src/main/java/io/github/retrooper/packetevents/enums/ClientVersion.java
+++ src/main/java/io/github/retrooper/packetevents/enums/ClientVersion.java
@@ -4,76 +4,50 @@ public enum ClientVersion {
     LESS_THAN_V_1_7_10, v_1_7_10, v_1_8, v_1_9, v_1_9_1, v_1_9_2, v_1_9_3, v_1_10, v_1_11, v_1_11_1, v_1_12, v_1_12_1, v_1_12_2,
     v_1_13, v_1_13_1, v_1_13_2, v_1_14, v_1_14_1, v_1_14_2, v_1_14_3, v_1_14_4, v_1_15, v_1_15_1, v_1_15_2, v_1_16, v_1_16_1, HIGHER_THAN_V_1_16_1, INVALID, ACCESS_FAILURE;
 
+    private static final Map<Integer, ClientVersion> VERSION_LOOKUP = new HashMap<>();
+    
+    public static void prepareLookUp() {
+        VERSION_LOOKUP.put(-1, ACCESS_FAILURE);
+        VERSION_LOOKUP.put(1, LESS_THAN_V_1_7_10);
+        VERSION_LOOKUP.put(2, LESS_THAN_V_1_7_10);
+        VERSION_LOOKUP.put(3, LESS_THAN_V_1_7_10);
+        VERSION_LOOKUP.put(4, LESS_THAN_V_1_7_10);
+        VERSION_LOOKUP.put(5, v_1_7_10);
+        VERSION_LOOKUP.put(47, v_1_8);
+        VERSION_LOOKUP.put(107, v_1_9);
+        VERSION_LOOKUP.put(108, v_1_9_1);
+        VERSION_LOOKUP.put(109, v_1_9_2);
+        VERSION_LOOKUP.put(110, v_1_9_3);
+        VERSION_LOOKUP.put(210, v_1_10);
+        VERSION_LOOKUP.put(315, v_1_11);
+        VERSION_LOOKUP.put(316, v_1_11_1);
+        VERSION_LOOKUP.put(335, v_1_12);
+        VERSION_LOOKUP.put(338, v_1_12_1);
+        VERSION_LOOKUP.put(340, v_1_12_2);
+        VERSION_LOOKUP.put(393, v_1_13);
+        VERSION_LOOKUP.put(401, v_1_13_1);
+        VERSION_LOOKUP.put(404, v_1_13_2);
+        VERSION_LOOKUP.put(477, v_1_14);
+        VERSION_LOOKUP.put(480, v_1_14_1);
+        VERSION_LOOKUP.put(485, v_1_14_2);
+        VERSION_LOOKUP.put(490, v_1_14_3);
+        VERSION_LOOKUP.put(498, v_1_14_4);
+        VERSION_LOOKUP.put(573, v_1_15);
+        VERSION_LOOKUP.put(575, v_1_15_1);
+        VERSION_LOOKUP.put(578, v_1_15_2);
+        VERSION_LOOKUP.put(735, v_1_16);
+        VERSION_LOOKUP.put(736, v_1_16_1);
+    }
+    
     public static ClientVersion fromProtocolVersion(int protocolVersion) {
         if(protocolVersion == -1 && PacketEvents.getSettings().doAutoResolveClientProtocolVersion()) {
            protocolVersion = ServerVersion.getVersion().toProtocolVersion();
         }
-        int remainder = -1;
-        switch (protocolVersion) {
-            case -1:
-                return ACCESS_FAILURE;
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-                return LESS_THAN_V_1_7_10;
-            case 5:
-                return v_1_7_10;
-            case 47://ranging from 1.8->1.8.9
-                return v_1_8;
-            case 107:
-                return v_1_9;
-            case 108:
-                return v_1_9_1;
-            case 109:
-                return v_1_9_2;
-            case 110:
-                return v_1_9_3; //can be 1.9.3 or 1.9.4
-            case 210:
-                return v_1_10; //could be 1.10,1.10.1, 1.10.2
-            case 315:
-                return v_1_11;
-            case 316: //could be 11.1 or 11.2
-                return v_1_11_1;
-            case 335:
-                return v_1_12;
-            case 338:
-                return v_1_12_1;
-            case 340:
-                return v_1_12_2;
-            case 393:
-                return v_1_13;
-            case 401:
-                return v_1_13_1;
-            case 404:
-                return v_1_13_2;
-            case 477:
-                return v_1_14;
-            case 480:
-                return v_1_14_1;
-            case 485:
-                return v_1_14_2;
-            case 490:
-                return v_1_14_3;
-            case 498:
-                return v_1_14_4;
-            case 573:
-                return v_1_15;
-            case 575:
-                return v_1_15_1;
-            case 578:
-                return v_1_15_2;
-            case 735:
-                return v_1_16;
-            case 736:
-                return v_1_16_1;
-            default:
-                remainder = protocolVersion;
+        final ClientVersion lookedUp = VERSION_LOOKUP.get(protocolVersion);
+        if(lookedUp != null) {
+            return lookedUp;
         }
-        if (remainder > 736) {
-            return HIGHER_THAN_V_1_16_1;
-        }
-        return INVALID;
+        return protocolVersion > 736 ? HIGHER_THAN_V_1_16_1 : INVALID;
     }
 
     /**
