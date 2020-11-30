39e105c69c59163c3fab034316b400d25ea183f4
terrarier2111 <unconfigured@null.spigotmc.org>
2020-11-30 14:45:51 +0100

    Revert "Other improvements"
    This reverts commit 1a2145b4

--- src/main/java/io/github/retrooper/packetevents/utils/server/SystemOS.java
+++ src/main/java/io/github/retrooper/packetevents/utils/server/SystemOS.java
@@ -48,9 +48,8 @@ public enum SystemOS {
 
     public static String[] getOperatingSystemNames() {
         final SystemOS[] values = values();
-        final int length = values.length - 1;
-        final String[] arr = new String[length];
-        for (int i = 0; i < length; i++) {
+        final String[] arr = new String[values.length - 1];
+        for (int i = 0; i < values.length - 1; i++) {
             arr[i] = values[i].name();
         }
         return arr;
