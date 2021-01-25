957adbb3a21a76c13cdbafbe8e0b90751a6fa1da
retrooper <retrooperdev@gmail.com>
2021-01-25 10:56:57 +0100

    1.8 pre 1

--- src/main/java/io/github/retrooper/packetevents/PacketEvents.java
+++ src/main/java/io/github/retrooper/packetevents/PacketEvents.java
@@ -68,7 +68,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public final class PacketEvents implements Listener, EventManager {
     private static PacketEvents instance;
-    private final PEVersion version = new PEVersion(1, 7, 9);
+    private final PEVersion version = new PEVersion(1, 7, 9, 1);
     private final EventManager eventManager = new PEEventManager();
     private final PlayerUtils playerUtils = new PlayerUtils();
     private final ServerUtils serverUtils = new ServerUtils();
