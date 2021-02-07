6221ff0b8bd1062a0fd1fd649fa8bac96b1dfbf8
retrooper <retrooperdev@gmail.com>
2021-02-07 21:02:17 +0100

    1.8-PRE-3 pre-release

--- src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/namedsoundeffect/WrappedPacketOutNamedSoundEffect.java
+++ src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/namedsoundeffect/WrappedPacketOutNamedSoundEffect.java
@@ -1,15 +1,15 @@
 package io.github.retrooper.packetevents.packetwrappers.play.out.namedsoundeffect;
 
 import io.github.retrooper.packetevents.packetwrappers.NMSPacket;
-import io.github.retrooper.packetevents.packetwrappers.SendableWrapper;
 import io.github.retrooper.packetevents.packetwrappers.WrappedPacket;
 import io.github.retrooper.packetevents.utils.nms.NMSUtils;
 import io.github.retrooper.packetevents.utils.reflection.Reflection;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
 
 //TODO test
-class WrappedPacketOutNamedSoundEffect extends WrappedPacket implements SendableWrapper {
+class WrappedPacketOutNamedSoundEffect extends WrappedPacket {
     private static boolean soundEffectVarExists;
+
     public WrappedPacketOutNamedSoundEffect(NMSPacket packet) {
         super(packet);
     }
@@ -19,19 +19,13 @@ class WrappedPacketOutNamedSoundEffect extends WrappedPacket implements Sendable
         soundEffectVarExists = Reflection.getField(packet.getClass(), NMSUtils.soundEffectClass, 0) != null;
     }
 
-    @Override
-    public Object asNMSPacket() {
-        return null;
-    }
-
     public String getSoundEffectName() {
         if (soundEffectVarExists) {
             Object soundEffect = readObject(0, NMSUtils.soundEffectClass);
             WrappedPacket soundEffectWrapper = new WrappedPacket(new NMSPacket(soundEffect));
             Object minecraftKey = soundEffectWrapper.readObject(0, NMSUtils.minecraftKeyClass);
             return NMSUtils.getStringFromMinecraftKey(minecraftKey);
-        }
-        else {
+        } else {
             return readString(0);
         }
     }
@@ -49,15 +43,14 @@ class WrappedPacketOutNamedSoundEffect extends WrappedPacket implements Sendable
     }
 
     //Might be more than 1.0 on some older versions
-    public float getVolume(){
+    public float getVolume() {
         return readFloat(0);
     }
 
     public float getPitch() {
         if (version.isOlderThan(ServerVersion.v_1_10)) {
             return readInt(1);
-        }
-        else {
+        } else {
             return readFloat(1);
         }
     }
