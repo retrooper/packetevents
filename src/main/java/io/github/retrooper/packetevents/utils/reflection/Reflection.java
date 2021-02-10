a4daa60a7f8599db778c445a409262ca79d1cb27
true-cc <creepercannyon@gmail.com>
2021-02-10 13:16:44 -0800

    Removed the WChat shit and added Reflection.getDeclaredMethodsInOrder

--- src/main/java/io/github/retrooper/packetevents/utils/reflection/Reflection.java
+++ src/main/java/io/github/retrooper/packetevents/utils/reflection/Reflection.java
@@ -25,9 +25,11 @@
 package io.github.retrooper.packetevents.utils.reflection;
 
 
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public final class Reflection {
     //FIELDS
@@ -82,13 +84,9 @@ public final class Reflection {
     }
 
     //METHODS
-    public static Method[] getMethods(final Class<?> cls) {
-        return cls.getDeclaredMethods();
-    }
-
     public static Method getMethod(final Class<?> cls, final int index, final Class<?>... params) {
         int currentIndex = 0;
-        for (final Method m : getMethods(cls)) {
+        for (final Method m : getDeclaredMethodsInOrder(cls)) {
             if (Arrays.equals(m.getParameterTypes(), params) && index == currentIndex++) {
                 m.setAccessible(true);
                 return m;
@@ -102,7 +100,7 @@ public final class Reflection {
 
     public static Method getMethod(Class<?> cls, Class<?> returning, int index, Class<?>... params) {
         int currentIndex = 0;
-        for (Method m : getMethods(cls)) {
+        for (Method m : getDeclaredMethodsInOrder(cls)) {
             if (Arrays.equals(m.getParameterTypes(), params)
                     && (returning == null || m.getReturnType().equals(returning))
                     && index == currentIndex++) {
@@ -117,7 +115,7 @@ public final class Reflection {
     }
 
     public static Method getMethod(final Class<?> cls, final String name, Class<?> returning, Class<?>... params) {
-        for (final Method m : getMethods(cls)) {
+        for (final Method m : getDeclaredMethodsInOrder(cls)) {
             if (m.getName().equals(name)
                     && Arrays.equals(m.getParameterTypes(), params) &&
                     (returning == null || m.getReturnType().equals(returning))) {
@@ -133,7 +131,7 @@ public final class Reflection {
 
     public static Method getMethod(final Class<?> cls, final String name, final int index) {
         int currentIndex = 0;
-        for (final Method m : getMethods(cls)) {
+        for (final Method m : getDeclaredMethodsInOrder(cls)) {
             if (m.getName().equals(name) && index == currentIndex++) {
                 m.setAccessible(true);
                 return m;
@@ -147,7 +145,7 @@ public final class Reflection {
 
     public static Method getMethod(final Class<?> cls, final Class<?> returning, final int index) {
         int currentIndex = 0;
-        for (final Method m : getMethods(cls)) {
+        for (final Method m : getDeclaredMethodsInOrder(cls)) {
             if ((returning == null || m.getReturnType().equals(returning)) && index == currentIndex++) {
                 m.setAccessible(true);
                 return m;
@@ -160,7 +158,7 @@ public final class Reflection {
     }
 
     public static Method getMethod(final Class<?> cls, final String name, final Class<?> returning) {
-        for (final Method m : getMethods(cls)) {
+        for (final Method m : getDeclaredMethodsInOrder(cls)) {
             if (m.getName().equals(name)
                     && (returning == null || m.getReturnType().equals(returning))) {
                 m.setAccessible(true);
@@ -171,5 +169,116 @@ public final class Reflection {
             return getMethod(cls.getSuperclass(), name, returning);
         }
         return null;
+    }
+    
+    // KissReflect
+    // thx https://github.com/wmacevoy/kiss/blob/master/src/main/java/kiss/util/Reflect.java
+
+    private static class MethodOffset implements Comparable<MethodOffset> {
+        MethodOffset(Method _method, int _offset) {
+            method = _method;
+            offset = _offset;
+        }
+
+        @Override
+        public int compareTo(MethodOffset target) {
+            return offset - target.offset;
+        }
+
+        Method method;
+        int offset;
+    }
+
+    static class ByLength implements Comparator<Method> {
+
+        @Override
+        public int compare(Method a, Method b) {
+            return b.getName().length() - a.getName().length();
+        }
+    }
+
+    /**
+     * Grok the bytecode to get the declared order
+     * <p>
+     * This method is slow; please store the method you want as to not use this again.
+     */
+    public static Method[] getDeclaredMethodsInOrder(Class<?> clazz) {
+        Method[] methods = null;
+        try {
+            String resource = clazz.getName().replace('.', '/') + ".class";
+
+            methods = clazz.getDeclaredMethods();
+
+            InputStream is = clazz.getClassLoader()
+              .getResourceAsStream(resource);
+
+            if (is == null) {
+                return methods;
+            }
+
+            java.util.Arrays.sort(methods, new ByLength());
+            ArrayList<byte[]> blocks = new ArrayList<>();
+            int length = 0;
+            for (; ; ) {
+                byte[] block = new byte[16 * 1024];
+                int n = is.read(block);
+                if (n > 0) {
+                    if (n < block.length) {
+                        block = java.util.Arrays.copyOf(block, n);
+                    }
+                    length += block.length;
+                    blocks.add(block);
+                } else {
+                    break;
+                }
+            }
+
+            byte[] data = new byte[length];
+            int offset = 0;
+            for (byte[] block : blocks) {
+                System.arraycopy(block, 0, data, offset, block.length);
+                offset += block.length;
+            }
+
+            String sdata = new String(data, StandardCharsets.UTF_8);
+            int lnt = sdata.indexOf("LineNumberTable");
+            if (lnt != -1) sdata = sdata.substring(lnt + "LineNumberTable".length() + 3);
+            int cde = sdata.lastIndexOf("SourceFile");
+            if (cde != -1) sdata = sdata.substring(0, cde);
+
+            MethodOffset[] mo = new MethodOffset[methods.length];
+
+
+            for (int i = 0; i < methods.length; ++i) {
+                int pos = -1;
+                for (; ; ) {
+                    pos = sdata.indexOf(methods[i].getName(), pos);
+                    if (pos == -1) break;
+                    boolean subset = false;
+                    for (int j = 0; j < i; ++j) {
+                        if (mo[j].offset >= 0 &&
+                          mo[j].offset <= pos &&
+                          pos < mo[j].offset + mo[j].method.getName().length()) {
+                            subset = true;
+                            break;
+                        }
+                    }
+                    if (subset) {
+                        pos += methods[i].getName().length();
+                    } else {
+                        break;
+                    }
+                }
+                mo[i] = new MethodOffset(methods[i], pos);
+            }
+            java.util.Arrays.sort(mo);
+            for (int i = 0; i < mo.length; ++i) {
+                methods[i] = mo[i].method;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return methods;
     }
 }
