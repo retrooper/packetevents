e8df865b3c6a3e6d9e2fbff805b3f26360d18a3c
retrooper <retrooperdev@gmail.com>
2021-02-22 21:51:27 +0100

    Completed WrappedPacketOutSetSlot wrapper, TODO: document all wrappers nicely including this one.

--- src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/setslot/WrappedPacketOutSetSlot.java
+++ src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/setslot/WrappedPacketOutSetSlot.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 retrooper
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.github.retrooper.packetevents.packetwrappers.play.out.setslot;
 
 import io.github.retrooper.packetevents.packettype.PacketTypeClasses;
@@ -10,19 +34,30 @@ import org.bukkit.inventory.ItemStack;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * @author AoElite
+ * @since 1.8
+ */
 public class WrappedPacketOutSetSlot extends WrappedPacket implements SendableWrapper {
 
     private static Constructor<?> packetConstructor;
+
+    private int windowID;
+    private int slot;
+    private ItemStack itemStack;
 
     public WrappedPacketOutSetSlot(NMSPacket packet) {
         super(packet);
     }
 
-    public WrappedPacketOutSetSlot(int windowId, int slot, ItemStack item) {
-        this.windowId = windowId;
+    public WrappedPacketOutSetSlot(int windowID, int slot, ItemStack itemstack) {
+        this.windowID = windowID;
         this.slot = slot;
-        this.item = item;
+        this.itemStack = itemstack;
     }
+
+
+
 
     @Override
     protected void load() {
@@ -34,15 +69,12 @@ public class WrappedPacketOutSetSlot extends WrappedPacket implements SendableWr
         }
     }
 
-    private int windowId;
-    private int slot;
-    private ItemStack item;
 
     public int getWindowId() {
         if (packet != null) {
             return readInt(0);
         } else {
-            return windowId;
+            return windowID;
         }
     }
 
@@ -54,19 +86,46 @@ public class WrappedPacketOutSetSlot extends WrappedPacket implements SendableWr
         }
     }
 
-    public ItemStack getItem() {
+    public ItemStack getItemStack() {
         if (packet != null) {
             Object nmsItemStack = readObject(0, NMSUtils.nmsItemStackClass);
             return NMSUtils.toBukkitItemStack(nmsItemStack);
         } else {
-            return item;
+            return itemStack;
+        }
+    }
+
+    public void setWindowId(int windowID) {
+        if (packet != null) {
+            writeInt(0, windowID);
+        }
+        else {
+            this.windowID = windowID;
+        }
+    }
+
+    public void setSlot(int slot) {
+        if (packet != null) {
+            writeInt(1, slot);
+        }
+        else {
+            this.slot = slot;
+        }
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        if (packet != null) {
+            writeObject(0, NMSUtils.toNMSItemStack(itemStack));
+        }
+        else {
+            this.itemStack = itemStack;
         }
     }
 
     @Override
     public Object asNMSPacket() {
         try {
-            return packetConstructor.newInstance(getWindowId(), getSlot(), NMSUtils.toNMSItemStack(getItem()));
+            return packetConstructor.newInstance(getWindowId(), getSlot(), NMSUtils.toNMSItemStack(getItemStack()));
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
