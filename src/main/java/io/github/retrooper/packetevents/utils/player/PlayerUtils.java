710508ae1ecab26d2a605c8330ba209ae9ddb6fb
retrooper <retrooperdev@gmail.com>
2021-02-23 23:33:52 +0100

    Revert "Merge pull request #105 from LIWKK/patch-25"
    This reverts commit 7d048c21b32581cc348f81484f263208fa02e8ce, reversing
    changes made to b59f0c5613653778f4d31ed78fe37ae97e1639b6.

--- src/main/java/io/github/retrooper/packetevents/utils/player/PlayerUtils.java
+++ src/main/java/io/github/retrooper/packetevents/utils/player/PlayerUtils.java
@@ -105,16 +105,6 @@ public final class PlayerUtils {
     public void ejectPlayer(final Player player) {
         PacketEvents.get().packetManager.ejectPlayer(player);
     }
-    
-     /**
-     * Check if player is injected inside PacketEvents.
-     *
-     * @param player  Target player.
-     */
-
-    public boolean isPlayerInjected(final Player player) {
-        return PacketEvents.get().injector.hasInjected(player);
-    }
 
     /**
      * Send a {@link SendableWrapper} wrapper to a player.
