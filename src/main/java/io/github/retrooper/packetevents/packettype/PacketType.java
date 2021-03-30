5a3ce13d930206bd5720953470cc28a8fcefef02
retrooper <retrooperdev@gmail.com>
2021-03-30 18:28:32 +0200

    PacketType#packetIDMap is no longer initialized with reflection, but manually hopefully reducing issues with obfuscators.

--- src/main/java/io/github/retrooper/packetevents/packettype/PacketType.java
+++ src/main/java/io/github/retrooper/packetevents/packettype/PacketType.java
@@ -25,13 +25,10 @@
 package io.github.retrooper.packetevents.packettype;
 
 import io.github.retrooper.packetevents.PacketEvents;
-import io.github.retrooper.packetevents.utils.reflection.Reflection;
-import io.github.retrooper.packetevents.utils.reflection.SubclassUtil;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Packet Type IDs.
@@ -47,28 +44,19 @@ public class PacketType {
      * This value may change over the versions so it is important to use the variable and not hard code its value.
      */
     public static final byte INVALID = -128;
-    public static final Map<Class<?>, Byte> packetIDMap = new HashMap<>();
+    public static final Map<Class<?>, Byte> packetIDMap = new ConcurrentHashMap<>();
 
     public static void load() {
-        for (Class<?> cls : PacketType.class.getDeclaredClasses()) {
-            for (Class<?> boundCls : cls.getDeclaredClasses()) {
-                for (Field f : boundCls.getFields()) {
-                    if (f.getType().equals(byte.class)) {
-                        try {
-                            byte byteID = f.getByte(null);
-                            Class<?> packetTypeClassesState = SubclassUtil.getSubClass(PacketTypeClasses.class, cls.getSimpleName());
-                            Class<?> packetTypeClassesStateBinding = SubclassUtil.getSubClass(packetTypeClassesState, boundCls.getSimpleName());
-                            Class<?> packetClass = (Class<?>) packetTypeClassesStateBinding.getDeclaredField(f.getName()).get(null);
-                            packetIDMap.put(packetClass, byteID);
-                        } catch (IllegalAccessException | NoSuchFieldException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }
+        Status.Client.load();
+        Status.Server.load();
 
+        Handshaking.Client.load();
 
+        Login.Client.load();
+        Login.Server.load();
+
+        Play.Client.load();
+        Play.Server.load();
     }
 
     /**
@@ -88,6 +76,10 @@ public class PacketType {
          */
         public static class Client {
             public static final byte START = -127, PING = -126;
+            private static void load() {
+                packetIDMap.put(PacketTypeClasses.Status.Client.START, Client.START);
+                packetIDMap.put(PacketTypeClasses.Status.Client.PING, Client.PING);
+            }
         }
 
         /**
@@ -99,6 +91,10 @@ public class PacketType {
          */
         public static class Server {
             public static final byte PONG = -125, SERVER_INFO = -124;
+            private static void load() {
+                packetIDMap.put(PacketTypeClasses.Status.Server.PONG, Server.PONG);
+                packetIDMap.put(PacketTypeClasses.Status.Server.SERVER_INFO, Server.SERVER_INFO);
+            }
         }
     }
 
@@ -118,6 +114,9 @@ public class PacketType {
          */
         public static class Client {
             public static final byte SET_PROTOCOL = -123;
+            private static void load() {
+                packetIDMap.put(PacketTypeClasses.Handshaking.Client.SET_PROTOCOL, Client.SET_PROTOCOL);
+            }
         }
     }
 
@@ -138,6 +137,11 @@ public class PacketType {
          */
         public static class Client {
             public static final byte CUSTOM_PAYLOAD = -122, START = -121, ENCRYPTION_BEGIN = -120;
+            private static void load() {
+                packetIDMap.put(PacketTypeClasses.Login.Client.CUSTOM_PAYLOAD, Client.CUSTOM_PAYLOAD);
+                packetIDMap.put(PacketTypeClasses.Login.Client.START, Client.START);
+                packetIDMap.put(PacketTypeClasses.Login.Client.ENCRYPTION_BEGIN, Client.ENCRYPTION_BEGIN);
+            }
         }
 
         /**
@@ -149,6 +153,13 @@ public class PacketType {
          */
         public static class Server {
             public static final byte CUSTOM_PAYLOAD = -119, DISCONNECT = -118, ENCRYPTION_BEGIN = -117, SUCCESS = -116, SET_COMPRESSION = -115;
+            private static void load() {
+                packetIDMap.put(PacketTypeClasses.Login.Server.CUSTOM_PAYLOAD, Server.CUSTOM_PAYLOAD);
+                packetIDMap.put(PacketTypeClasses.Login.Server.DISCONNECT, Server.DISCONNECT);
+                packetIDMap.put(PacketTypeClasses.Login.Server.ENCRYPTION_BEGIN, Server.ENCRYPTION_BEGIN);
+                packetIDMap.put(PacketTypeClasses.Login.Server.SUCCESS, Server.SUCCESS);
+                packetIDMap.put(PacketTypeClasses.Login.Server.SET_COMPRESSION, Server.SET_COMPRESSION);
+            }
         }
     }
 
@@ -181,6 +192,56 @@ public class PacketType {
                     SET_COMMAND_BLOCK = -77, SET_COMMAND_MINECART = -76, SET_CREATIVE_SLOT = -75, SET_JIGSAW = -74,
                     STRUCT = -73, UPDATE_SIGN = -72, ARM_ANIMATION = -71, SPECTATE = -70,
                     USE_ITEM = -69, BLOCK_PLACE = -68;
+
+            private static void load() {
+                packetIDMap.put(PacketTypeClasses.Play.Client.TELEPORT_ACCEPT, TELEPORT_ACCEPT);
+                packetIDMap.put(PacketTypeClasses.Play.Client.TILE_NBT_QUERY, TILE_NBT_QUERY);
+                packetIDMap.put(PacketTypeClasses.Play.Client.DIFFICULTY_CHANGE, DIFFICULTY_CHANGE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.CHAT, CHAT);
+                packetIDMap.put(PacketTypeClasses.Play.Client.CLIENT_COMMAND, CLIENT_COMMAND);
+                packetIDMap.put(PacketTypeClasses.Play.Client.SETTINGS, SETTINGS);
+                packetIDMap.put(PacketTypeClasses.Play.Client.TAB_COMPLETE, TAB_COMPLETE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.TRANSACTION, TRANSACTION);
+                packetIDMap.put(PacketTypeClasses.Play.Client.ENCHANT_ITEM, ENCHANT_ITEM);
+                packetIDMap.put(PacketTypeClasses.Play.Client.WINDOW_CLICK, WINDOW_CLICK);
+                packetIDMap.put(PacketTypeClasses.Play.Client.CLOSE_WINDOW, CLOSE_WINDOW);
+                packetIDMap.put(PacketTypeClasses.Play.Client.CUSTOM_PAYLOAD, CUSTOM_PAYLOAD);
+                packetIDMap.put(PacketTypeClasses.Play.Client.B_EDIT, B_EDIT);
+                packetIDMap.put(PacketTypeClasses.Play.Client.ENTITY_NBT_QUERY, ENTITY_NBT_QUERY);
+                packetIDMap.put(PacketTypeClasses.Play.Client.USE_ENTITY, USE_ENTITY);
+                packetIDMap.put(PacketTypeClasses.Play.Client.JIGSAW_GENERATE, JIGSAW_GENERATE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.KEEP_ALIVE, KEEP_ALIVE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.DIFFICULTY_LOCK, DIFFICULTY_LOCK);
+                packetIDMap.put(PacketTypeClasses.Play.Client.POSITION, POSITION);
+                packetIDMap.put(PacketTypeClasses.Play.Client.POSITION_LOOK, POSITION_LOOK);
+                packetIDMap.put(PacketTypeClasses.Play.Client.LOOK, LOOK);
+                packetIDMap.put(PacketTypeClasses.Play.Client.FLYING, FLYING);
+                packetIDMap.put(PacketTypeClasses.Play.Client.VEHICLE_MOVE, VEHICLE_MOVE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.BOAT_MOVE, BOAT_MOVE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.PICK_ITEM, PICK_ITEM);
+                packetIDMap.put(PacketTypeClasses.Play.Client.AUTO_RECIPE, AUTO_RECIPE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.ABILITIES, ABILITIES);
+                packetIDMap.put(PacketTypeClasses.Play.Client.BLOCK_DIG, BLOCK_DIG);
+                packetIDMap.put(PacketTypeClasses.Play.Client.ENTITY_ACTION, ENTITY_ACTION);
+                packetIDMap.put(PacketTypeClasses.Play.Client.STEER_VEHICLE, STEER_VEHICLE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.RECIPE_DISPLAYED, RECIPE_DISPLAYED);
+                packetIDMap.put(PacketTypeClasses.Play.Client.ITEM_NAME, ITEM_NAME);
+                packetIDMap.put(PacketTypeClasses.Play.Client.RESOURCE_PACK_STATUS, RESOURCE_PACK_STATUS);
+                packetIDMap.put(PacketTypeClasses.Play.Client.ADVANCEMENTS, ADVANCEMENTS);
+                packetIDMap.put(PacketTypeClasses.Play.Client.TR_SEL, TR_SEL);
+                packetIDMap.put(PacketTypeClasses.Play.Client.BEACON, BEACON);
+                packetIDMap.put(PacketTypeClasses.Play.Client.HELD_ITEM_SLOT, HELD_ITEM_SLOT);
+                packetIDMap.put(PacketTypeClasses.Play.Client.SET_COMMAND_BLOCK, SET_COMMAND_BLOCK);
+                packetIDMap.put(PacketTypeClasses.Play.Client.SET_COMMAND_MINECART, SET_COMMAND_MINECART);
+                packetIDMap.put(PacketTypeClasses.Play.Client.SET_CREATIVE_SLOT, SET_CREATIVE_SLOT);
+                packetIDMap.put(PacketTypeClasses.Play.Client.SET_JIGSAW, SET_JIGSAW);
+                packetIDMap.put(PacketTypeClasses.Play.Client.STRUCT, STRUCT);
+                packetIDMap.put(PacketTypeClasses.Play.Client.UPDATE_SIGN, UPDATE_SIGN);
+                packetIDMap.put(PacketTypeClasses.Play.Client.ARM_ANIMATION, ARM_ANIMATION);
+                packetIDMap.put(PacketTypeClasses.Play.Client.SPECTATE, SPECTATE);
+                packetIDMap.put(PacketTypeClasses.Play.Client.USE_ITEM, USE_ITEM);
+                packetIDMap.put(PacketTypeClasses.Play.Client.BLOCK_PLACE, BLOCK_PLACE);
+            }
 
             /**
              * Server-bound Play Packet Type utility.
@@ -258,6 +319,106 @@ public class PacketType {
                     ENTITY_SOUND = 14, NAMED_SOUND_EFFECT = 15, STOP_SOUND = 16, PLAYER_LIST_HEADER_FOOTER = 17,
                     NBT_QUERY = 18, COLLECT = 19, ENTITY_TELEPORT = 20, ADVANCEMENTS = 21, UPDATE_ATTRIBUTES = 22,
                     ENTITY_EFFECT = 23, RECIPE_UPDATE = 24, TAGS = 25, MAP_CHUNK_BULK = 26, NAMED_ENTITY_SPAWN = 27;
+
+            private static void load() {
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_ENTITY, SPAWN_ENTITY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_ENTITY_EXPERIENCE_ORB, SPAWN_ENTITY_EXPERIENCE_ORB);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_ENTITY_WEATHER, SPAWN_ENTITY_WEATHER);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_ENTITY_LIVING, SPAWN_ENTITY_LIVING);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_ENTITY_PAINTING, SPAWN_ENTITY_PAINTING);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_ENTITY_SPAWN, SPAWN_ENTITY_SPAWN);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ANIMATION, ANIMATION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.STATISTIC, STATISTIC);
+                packetIDMap.put(PacketTypeClasses.Play.Server.BLOCK_BREAK, BLOCK_BREAK);
+                packetIDMap.put(PacketTypeClasses.Play.Server.BLOCK_BREAK_ANIMATION, BLOCK_BREAK_ANIMATION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.TILE_ENTITY_DATA, TILE_ENTITY_DATA);
+                packetIDMap.put(PacketTypeClasses.Play.Server.BLOCK_ACTION, BLOCK_ACTION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.BLOCK_CHANGE, BLOCK_CHANGE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.BOSS, BOSS);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_ENTITY, SPAWN_ENTITY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SERVER_DIFFICULTY, SERVER_DIFFICULTY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.CHAT, CHAT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.MULTI_BLOCK_CHANGE, MULTI_BLOCK_CHANGE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.TAB_COMPLETE, TAB_COMPLETE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.COMMANDS, COMMANDS);
+                packetIDMap.put(PacketTypeClasses.Play.Server.TRANSACTION, TRANSACTION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.CLOSE_WINDOW, CLOSE_WINDOW);
+                packetIDMap.put(PacketTypeClasses.Play.Server.WINDOW_ITEMS, WINDOW_ITEMS);
+                packetIDMap.put(PacketTypeClasses.Play.Server.WINDOW_DATA, WINDOW_DATA);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SET_SLOT, SET_SLOT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SET_COOLDOWN, SET_COOLDOWN);
+                packetIDMap.put(PacketTypeClasses.Play.Server.CUSTOM_PAYLOAD, CUSTOM_PAYLOAD);
+                packetIDMap.put(PacketTypeClasses.Play.Server.CUSTOM_SOUND_EFFECT, CUSTOM_SOUND_EFFECT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.KICK_DISCONNECT, KICK_DISCONNECT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_STATUS, ENTITY_STATUS);
+                packetIDMap.put(PacketTypeClasses.Play.Server.EXPLOSION, EXPLOSION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.UNLOAD_CHUNK, UNLOAD_CHUNK);
+                packetIDMap.put(PacketTypeClasses.Play.Server.GAME_STATE_CHANGE, GAME_STATE_CHANGE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.OPEN_WINDOW_HORSE, OPEN_WINDOW_HORSE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.KEEP_ALIVE, KEEP_ALIVE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.MAP_CHUNK, MAP_CHUNK);
+                packetIDMap.put(PacketTypeClasses.Play.Server.WORLD_EVENT, WORLD_EVENT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.WORLD_EVENT, SPAWN_ENTITY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.WORLD_PARTICLES, WORLD_PARTICLES);
+                packetIDMap.put(PacketTypeClasses.Play.Server.LIGHT_UPDATE, LIGHT_UPDATE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.LOGIN, LOGIN);
+                packetIDMap.put(PacketTypeClasses.Play.Server.MAP, MAP);
+                packetIDMap.put(PacketTypeClasses.Play.Server.OPEN_WINDOW_MERCHANT, OPEN_WINDOW_MERCHANT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.REL_ENTITY_MOVE, REL_ENTITY_MOVE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.REL_ENTITY_MOVE_LOOK, REL_ENTITY_MOVE_LOOK);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_LOOK, ENTITY_LOOK);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY, ENTITY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.VEHICLE_MOVE, VEHICLE_MOVE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.OPEN_BOOK, OPEN_BOOK);
+                packetIDMap.put(PacketTypeClasses.Play.Server.OPEN_WINDOW, OPEN_WINDOW);
+                packetIDMap.put(PacketTypeClasses.Play.Server.OPEN_SIGN_EDITOR, OPEN_SIGN_EDITOR);
+                packetIDMap.put(PacketTypeClasses.Play.Server.AUTO_RECIPE, AUTO_RECIPE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ABILITIES, ABILITIES);
+                packetIDMap.put(PacketTypeClasses.Play.Server.COMBAT_EVENT, COMBAT_EVENT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.PLAYER_INFO, PLAYER_INFO);
+                packetIDMap.put(PacketTypeClasses.Play.Server.LOOK_AT, LOOK_AT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.POSITION, POSITION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.RECIPES, RECIPES);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_DESTROY, ENTITY_DESTROY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.REMOVE_ENTITY_EFFECT, REMOVE_ENTITY_EFFECT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.RESOURCE_PACK_SEND, RESOURCE_PACK_SEND);
+                packetIDMap.put(PacketTypeClasses.Play.Server.RESPAWN, RESPAWN);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_HEAD_ROTATION, ENTITY_HEAD_ROTATION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SELECT_ADVANCEMENT_TAB, SELECT_ADVANCEMENT_TAB);
+                packetIDMap.put(PacketTypeClasses.Play.Server.WORLD_BORDER, WORLD_BORDER);
+                packetIDMap.put(PacketTypeClasses.Play.Server.CAMERA, CAMERA);
+                packetIDMap.put(PacketTypeClasses.Play.Server.HELD_ITEM_SLOT, HELD_ITEM_SLOT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.VIEW_CENTRE, VIEW_CENTRE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.VIEW_DISTANCE, VIEW_DISTANCE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SCOREBOARD_DISPLAY_OBJECTIVE, SCOREBOARD_DISPLAY_OBJECTIVE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_METADATA, ENTITY_METADATA);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ATTACH_ENTITY, ATTACH_ENTITY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_VELOCITY, ENTITY_VELOCITY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_EQUIPMENT, ENTITY_EQUIPMENT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.EXPERIENCE, EXPERIENCE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.UPDATE_HEALTH, UPDATE_HEALTH);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SCOREBOARD_OBJECTIVE, SCOREBOARD_OBJECTIVE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.MOUNT, MOUNT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SCOREBOARD_TEAM, SCOREBOARD_TEAM);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SCOREBOARD_SCORE, SCOREBOARD_SCORE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.SPAWN_POSITION, SPAWN_POSITION);
+                packetIDMap.put(PacketTypeClasses.Play.Server.UPDATE_TIME, UPDATE_TIME);
+                packetIDMap.put(PacketTypeClasses.Play.Server.TITLE, TITLE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_SOUND, ENTITY_SOUND);
+                packetIDMap.put(PacketTypeClasses.Play.Server.NAMED_SOUND_EFFECT, NAMED_SOUND_EFFECT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.STOP_SOUND, STOP_SOUND);
+                packetIDMap.put(PacketTypeClasses.Play.Server.PLAYER_LIST_HEADER_FOOTER, PLAYER_LIST_HEADER_FOOTER);
+                packetIDMap.put(PacketTypeClasses.Play.Server.NBT_QUERY, NBT_QUERY);
+                packetIDMap.put(PacketTypeClasses.Play.Server.COLLECT, COLLECT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_TELEPORT, ENTITY_TELEPORT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ADVANCEMENTS, ADVANCEMENTS);
+                packetIDMap.put(PacketTypeClasses.Play.Server.UPDATE_ATTRIBUTES, UPDATE_ATTRIBUTES);
+                packetIDMap.put(PacketTypeClasses.Play.Server.ENTITY_EFFECT, ENTITY_EFFECT);
+                packetIDMap.put(PacketTypeClasses.Play.Server.RECIPE_UPDATE, RECIPE_UPDATE);
+                packetIDMap.put(PacketTypeClasses.Play.Server.TAGS, TAGS);
+                packetIDMap.put(PacketTypeClasses.Play.Server.MAP_CHUNK_BULK, MAP_CHUNK_BULK);
+                packetIDMap.put(PacketTypeClasses.Play.Server.NAMED_ENTITY_SPAWN, NAMED_ENTITY_SPAWN);
+            }
 
             /**
              * Client-bound Play Packet Type utility.
