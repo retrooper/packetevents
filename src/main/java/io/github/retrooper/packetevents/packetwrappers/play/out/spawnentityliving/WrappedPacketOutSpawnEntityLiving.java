189185e8e188380b3f103f56ce6e8cb5e198a801
retrooper <retrooperdev@gmail.com>
2021-05-22 22:44:44 +0200

    WrappedPacketOutSpawnEntityLiving completed, but untested.

--- src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/spawnentityliving/WrappedPacketOutSpawnEntityLiving.java
+++ src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/spawnentityliving/WrappedPacketOutSpawnEntityLiving.java
@@ -18,31 +18,124 @@
 
 package io.github.retrooper.packetevents.packetwrappers.play.out.spawnentityliving;
 
+import io.github.retrooper.packetevents.packettype.PacketTypeClasses;
 import io.github.retrooper.packetevents.packetwrappers.NMSPacket;
+import io.github.retrooper.packetevents.packetwrappers.api.SendableWrapper;
 import io.github.retrooper.packetevents.packetwrappers.api.helper.WrappedPacketEntityAbstraction;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
 import io.github.retrooper.packetevents.utils.vector.Vector3d;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 
+import java.lang.reflect.Constructor;
 import java.util.Optional;
 import java.util.UUID;
 
-//TODO finish wrapper
-class WrappedPacketOutSpawnEntityLiving extends WrappedPacketEntityAbstraction {
+public class WrappedPacketOutSpawnEntityLiving extends WrappedPacketEntityAbstraction implements SendableWrapper {
+    private static final float rotationFactor = 256.0F / 360.0F;
+    private static final double velocityFactor = 8000.0D;
+    private static Constructor<?> packetDefaultConstructor;
+
+    private Vector3d position, velocity;
+    private EntityType entityType;
+    private UUID uuid;
+    private float yaw, pitch, headPitch;
+
     public WrappedPacketOutSpawnEntityLiving(NMSPacket packet) {
         super(packet);
     }
 
+    public WrappedPacketOutSpawnEntityLiving(Entity entity, UUID uuid, EntityType entityType, Vector3d position, Vector3d velocity, float yaw, float pitch, float headPitch) {
+        setEntity(entity);
+        this.uuid = uuid;
+        this.entityType = entityType;
+        this.position = position;
+        this.velocity = velocity;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.headPitch = headPitch;
+    }
+
+    public WrappedPacketOutSpawnEntityLiving(int entityID, UUID uuid, EntityType entityType, Vector3d position, Vector3d velocity, float yaw, float pitch, float headPitch) {
+        setEntityId(entityID);
+        this.uuid = uuid;
+        this.entityType = entityType;
+        this.position = position;
+        this.velocity = velocity;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.headPitch = headPitch;
+    }
+
     @Override
     protected void load() {
-        net.minecraft.server.v1_7_R4.PacketPlayOutSpawnEntityLiving a0;
-        net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving a1;
-        net.minecraft.server.v1_9_R1.PacketPlayOutSpawnEntityLiving a2;
-        net.minecraft.server.v1_9_R2.PacketPlayOutSpawnEntityLiving a3;
-        net.minecraft.server.v1_12_R1.PacketPlayOutSpawnEntityLiving a4;
-        net.minecraft.server.v1_13_R1.PacketPlayOutSpawnEntityLiving a5;
-        net.minecraft.server.v1_13_R2.PacketPlayOutSpawnEntityLiving a6;
-        net.minecraft.server.v1_16_R2.PacketPlayOutSpawnEntityLiving a7;
+        try {
+            packetDefaultConstructor = PacketTypeClasses.Play.Server.SPAWN_ENTITY_LIVING.getConstructor();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Optional<UUID> getUUID() {
+        if (version.isNewerThan(ServerVersion.v_1_8_8)) {
+            if (packet != null) {
+                return Optional.of(readObject(0, UUID.class));
+            }
+            else {
+                return Optional.of(uuid);
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public void setUUID(UUID uuid) {
+        if (version.isNewerThan(ServerVersion.v_1_8_8)) {
+            if (packet != null) {
+                writeObject(0, uuid);
+            }
+            else {
+                this.uuid = uuid;
+            }
+        }
+    }
+
+    public EntityType getEntityType() {
+        if (packet != null) {
+            int entityTypeID = readInt(1);
+            return EntityType.fromId(entityTypeID);
+        } else {
+            return entityType;
+        }
+    }
+
+    public void setEntityType(EntityType entityType) {
+        if (packet != null) {
+            writeInt(1, entityType.getTypeId());
+        } else {
+            this.entityType = entityType;
+        }
+    }
+
+    public Vector3d getPosition() {
+        if (packet != null) {
+            double x;
+            double y;
+            double z;
+            //On 1.8.x and 1.7.10 the y is factored by 32
+            if (version.isOlderThan(ServerVersion.v_1_9)) {
+                x = readInt(2) / 32.0;
+                y = readInt(3) / 32.0;
+                z = readInt(4) / 32.0;
+            } else {
+                x = readDouble(0);
+                y = readDouble(1);
+                z = readDouble(2);
+            }
+            return new Vector3d(x, y, z);
+        } else {
+            return position;
+        }
     }
 
     private int floor(double a) {
@@ -50,57 +143,135 @@ class WrappedPacketOutSpawnEntityLiving extends WrappedPacketEntityAbstraction {
         return a < (double) result ? result - 1 : result;
     }
 
-    public Vector3d getPosition() {
-        double x;
-        double y;
-        double z;
-        //On 1.8.x and 1.7.10 the y is factored by 32
-        if (version.isOlderThan(ServerVersion.v_1_9)) {
-            x = readInt(2) / 32.0;
-            y = readInt(3) / 32.0;
-            z = readInt(4) / 32.0;
-        } else {
-            x = readDouble(0);
-            y = readDouble(1);
-            z = readDouble(2);
-        }
-        return new Vector3d(x, y, z);
-    }
-
     public void setPosition(Vector3d position) {
-        //On 1.8.x and 1.7.10 the y must be factored by 32
-        if (version.isOlderThan(ServerVersion.v_1_9)) {
-            writeInt(2, floor(position.x * 32.0));
-            writeInt(3, floor(position.y * 32.0));
-            writeInt(4, floor(position.z * 32.0));
+        if (packet != null) {
+            //On 1.8.x and 1.7.10 the y must be factored by 32
+            if (version.isOlderThan(ServerVersion.v_1_9)) {
+                writeInt(2, floor(position.x * 32.0));
+                writeInt(3, floor(position.y * 32.0));
+                writeInt(4, floor(position.z * 32.0));
+            } else {
+                writeDouble(0, position.x);
+                writeDouble(1, position.y);
+                writeDouble(2, position.z);
+            }
         } else {
-            writeDouble(0, position.x);
-            writeDouble(1, position.y);
-            writeDouble(2, position.z);
+            this.position = position;
         }
     }
 
-    public EntityType getType() {
-        int entityTypeID = readInt(1);
-        return EntityType.fromId(entityTypeID);
-    }
-
-    public void setEntityType(EntityType type) {
-        writeInt(1, type.getTypeId());
-    }
-
-    public Optional<UUID> getUUID() {
-        if (version.isNewerThan(ServerVersion.v_1_8_8)) {
-            return Optional.of(readObject(0, UUID.class));
+    public Vector3d getVelocity() {
+        if (packet != null) {
+            int factoredVelX;
+            int factoredVelY;
+            int factoredVelZ;
+            if (version.isOlderThan(ServerVersion.v_1_9)) {
+                factoredVelX = readInt(5);
+                factoredVelY = readInt(6);
+                factoredVelZ = readInt(7);
+            } else {
+                factoredVelX = readInt(2);
+                factoredVelY = readInt(3);
+                factoredVelZ = readInt(4);
+            }
+            return new Vector3d(factoredVelX / velocityFactor, factoredVelY / velocityFactor, factoredVelZ / velocityFactor);
         }
         else {
-            return Optional.empty();
+            return this.velocity;
         }
     }
 
-    public void setUUID(UUID uuid) {
-        if (version.isNewerThan(ServerVersion.v_1_8_8)) {
-            writeObject(0, uuid);
+    public void setVelocity(Vector3d velocity) {
+        if (packet != null) {
+            int factoredVelX = (int) (velocity.x * velocityFactor);
+            int factoredVelY = (int) (velocity.y * velocityFactor);
+            int factoredVelZ = (int) (velocity.z * velocityFactor);
+            if (version.isOlderThan(ServerVersion.v_1_9)) {
+                writeInt(5, factoredVelX);
+                writeInt(6, factoredVelY);
+                writeInt(7, factoredVelZ);
+            } else {
+                writeInt(2, factoredVelX);
+                writeInt(3, factoredVelY);
+                writeInt(4, factoredVelZ);
+            }
         }
+        else {
+            this.velocity = velocity;
+        }
+    }
+
+    public float getYaw() {
+        if (packet != null) {
+            byte factoredYaw = readByte(0);
+            return factoredYaw / rotationFactor;
+        }
+        else {
+            return yaw;
+        }
+    }
+
+    public void setYaw(float yaw) {
+        if (packet != null) {
+            writeByte(0, (byte) ((int) (yaw * rotationFactor)));
+        }
+        else {
+            this.yaw = yaw;
+        }
+    }
+
+    public float getPitch() {
+        if (packet != null) {
+            byte factoredPitch = readByte(1);
+            return factoredPitch / rotationFactor;
+        }
+        else {
+            return pitch;
+        }
+    }
+
+    public void setPitch(float pitch) {
+        if (packet != null) {
+            writeByte(1, (byte) ((int) (pitch * rotationFactor)));
+        }
+        else {
+            this.pitch = pitch;
+        }
+    }
+
+    public float getHeadPitch() {
+        if (packet != null) {
+            byte factoredHeadPitch = readByte(2);
+            return factoredHeadPitch / rotationFactor;
+        }
+        else {
+            return this.headPitch;
+        }
+    }
+
+    public void setHeadPitch(float headPitch) {
+        if (packet != null) {
+            writeByte(2, (byte) ((int) (headPitch * rotationFactor)));
+        }
+        else {
+            this.headPitch = headPitch;
+        }
+    }
+
+    @Override
+    public Object asNMSPacket() throws Exception {
+        Object packetInstance = packetDefaultConstructor.newInstance();
+        WrappedPacketOutSpawnEntityLiving wrappedPacketOutSpawnEntityLiving = new WrappedPacketOutSpawnEntityLiving(new NMSPacket(packetInstance));
+        wrappedPacketOutSpawnEntityLiving.setEntityId(getEntityId());
+        if (version.isNewerThan(ServerVersion.v_1_8_8)) {
+            wrappedPacketOutSpawnEntityLiving.setUUID(getUUID().orElse(null));
+        }
+        wrappedPacketOutSpawnEntityLiving.setEntityType(getEntityType());
+        wrappedPacketOutSpawnEntityLiving.setPosition(getPosition());
+        wrappedPacketOutSpawnEntityLiving.setVelocity(getVelocity());
+        wrappedPacketOutSpawnEntityLiving.setYaw(getYaw());
+        wrappedPacketOutSpawnEntityLiving.setPitch(getPitch());
+        wrappedPacketOutSpawnEntityLiving.setHeadPitch(getHeadPitch());
+        return packetInstance;
     }
 }
