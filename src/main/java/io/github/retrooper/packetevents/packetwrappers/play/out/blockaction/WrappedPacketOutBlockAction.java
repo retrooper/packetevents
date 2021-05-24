b1f64155dcceb65920aa7e3e5ce0c529bfd8114c
Tecnio <Tecnio@users.noreply.github.com>
2021-05-24 20:26:51 +0300

    Made BlockAction sendable.

--- src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/blockaction/WrappedPacketOutBlockAction.java
+++ src/main/java/io/github/retrooper/packetevents/packetwrappers/play/out/blockaction/WrappedPacketOutBlockAction.java
@@ -1,10 +1,14 @@
 package io.github.retrooper.packetevents.packetwrappers.play.out.blockaction;
 
+import io.github.retrooper.packetevents.packettype.PacketTypeClasses;
+import io.github.retrooper.packetevents.packetwrappers.NMSPacket;
 import io.github.retrooper.packetevents.packetwrappers.WrappedPacket;
 import io.github.retrooper.packetevents.packetwrappers.api.SendableWrapper;
 import io.github.retrooper.packetevents.utils.nms.NMSUtils;
 import io.github.retrooper.packetevents.utils.vector.Vector3i;
 import org.bukkit.Material;
+
+import java.lang.reflect.Constructor;
 
 /**
  * This packet is used for a number of actions and animations performed by blocks, usually non-persistent.
@@ -14,36 +18,121 @@ import org.bukkit.Material;
  */
 public class WrappedPacketOutBlockAction extends WrappedPacket implements SendableWrapper {
 
-    public Vector3i getBlockPosition() {
-        final Vector3i blockPos = new Vector3i();
+    private Vector3i blockPos;
+    private int action, actionParam;
+    private Material material;
 
-        final Object blockPosObj = readObject(1, NMSUtils.blockPosClass);
+    private static Constructor<?> packetConstructor;
 
+    public WrappedPacketOutBlockAction(final NMSPacket packet) {
+        super(packet);
+    }
+
+    public WrappedPacketOutBlockAction(final Vector3i blockPos, final int action, final int actionParam, final Material material) {
+        this.blockPos = blockPos;
+        this.action = action;
+        this.actionParam = actionParam;
+        this.material = material;
+    }
+
+    @Override
+    protected void load() {
         try {
-            blockPos.x = (int) NMSUtils.getBlockPosX.invoke(blockPosObj);
-            blockPos.y = (int) NMSUtils.getBlockPosY.invoke(blockPosObj);
-            blockPos.z = (int) NMSUtils.getBlockPosZ.invoke(blockPosObj);
+            packetConstructor = PacketTypeClasses.Play.Server.BLOCK_ACTION.getConstructors()[1];
         } catch (final Exception e) {
             e.printStackTrace();
         }
+    }
 
-        return blockPos;
+    public Vector3i getBlockPosition() {
+        if (packet != null) {
+            final Vector3i blockPos = new Vector3i();
+
+            final Object blockPosObj = readObject(0, NMSUtils.blockPosClass);
+
+            try {
+                blockPos.x = (int) NMSUtils.getBlockPosX.invoke(blockPosObj);
+                blockPos.y = (int) NMSUtils.getBlockPosY.invoke(blockPosObj);
+                blockPos.z = (int) NMSUtils.getBlockPosZ.invoke(blockPosObj);
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+
+            return blockPos;
+        } else {
+            return this.blockPos;
+        }
+    }
+
+    public void setBlockPosition(final Vector3i blockPos) {
+        if (packet != null) {
+            final Object nmsBlockPos = NMSUtils.generateNMSBlockPos(blockPos.x, blockPos.y, blockPos.z);
+            write(NMSUtils.blockPosClass, 0, nmsBlockPos);
+        } else {
+            this.blockPos = blockPos;
+        }
     }
 
     public int getActionId() {
-        return readInt(0);
+        if (packet != null) {
+            return readInt(0);
+        } else {
+            return this.action;
+        }
+    }
+
+    public void setActionId(final int actionId) {
+        if (packet != null) {
+            writeInt(0, actionId);
+        } else {
+            this.action = actionId;
+        }
     }
 
     public int getActionParam() {
-        return readInt(1);
+        if (packet != null) {
+            return readInt(1);
+        } else {
+            return this.actionParam;
+        }
+    }
+
+    public void setActionParam(final int actionParam) {
+        if (packet != null) {
+            writeInt(1, actionParam);
+        } else {
+            this.actionParam = actionParam;
+        }
     }
 
     public Material getBlockMaterial() {
-        return NMSUtils.getMaterialFromNMSBlock(readObject(0, NMSUtils.blockClass));
+        if (packet != null) {
+            return NMSUtils.getMaterialFromNMSBlock(readObject(0, NMSUtils.blockClass));
+        } else {
+            return this.material;
+        }
+    }
+
+    public void setBlock(final Material material) {
+        if (packet != null) {
+            final Object nmsBlock = NMSUtils.getNMSBlockFromMaterial(material);
+            write(NMSUtils.blockClass, 0, nmsBlock);
+        } else {
+            this.material = material;
+        }
     }
 
     @Override
     public Object asNMSPacket() throws Exception {
-        return null;
+        final Object packetInstance;
+
+        final Object nmsBlockPos = NMSUtils.generateNMSBlockPos(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+        final Object nmsBlock = NMSUtils.getNMSBlockFromMaterial(material);
+
+        packetInstance = packetConstructor.newInstance(
+            nmsBlockPos, nmsBlock, action, actionParam
+        );
+
+        return packetInstance;
     }
 }
