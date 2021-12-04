924b0c2b24a09c51d476ec91f3d587bcbdae4365
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-12-04 11:09:43 -0600

    WrapperPlayServerSetCooldown

---
+++ api/src/main/java/com/github/retrooper/packetevents/wrapper/play/server/WrapperPlayServerSetCooldown.java
@@ -0,0 +1,56 @@
+package com.github.retrooper.packetevents.wrapper.play.server;
+
+import com.github.retrooper.packetevents.event.impl.PacketSendEvent;
+import com.github.retrooper.packetevents.protocol.inventory.ItemType;
+import com.github.retrooper.packetevents.protocol.inventory.ItemTypes;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.PacketWrapper;
+
+public class WrapperPlayServerSetCooldown extends PacketWrapper<WrapperPlayServerSetCooldown> {
+    ItemType item;
+    int cooldownTicks;
+
+    public WrapperPlayServerSetCooldown(PacketSendEvent event) {
+        super(event);
+    }
+
+    public WrapperPlayServerSetCooldown(ItemType item, int cooldownTicks) {
+        super(PacketType.Play.Server.SET_SLOT);
+        this.item = item;
+        this.cooldownTicks = cooldownTicks;
+    }
+
+    @Override
+    public void readData() {
+        item = ItemTypes.getById(readVarInt());
+        cooldownTicks = readVarInt();
+    }
+
+    @Override
+    public void readData(WrapperPlayServerSetCooldown wrapper) {
+        item = wrapper.item;
+        cooldownTicks = wrapper.cooldownTicks;
+    }
+
+    @Override
+    public void writeData() {
+        writeVarInt(item.getId());
+        writeVarInt(cooldownTicks);
+    }
+
+    public ItemType getItem() {
+        return item;
+    }
+
+    public void setItem(ItemType item) {
+        this.item = item;
+    }
+
+    public int getCooldownTicks() {
+        return cooldownTicks;
+    }
+
+    public void setCooldownTicks(int cooldownTicks) {
+        this.cooldownTicks = cooldownTicks;
+    }
+}
