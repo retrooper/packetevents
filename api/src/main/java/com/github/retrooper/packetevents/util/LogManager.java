2377ca8e020b04661104be36be9b325497a3976c
retrooper <retrooperdev@gmail.com>
2021-12-13 07:21:12 +0100

    Minor improvements to cross-platform log manager.

--- api/src/main/java/com/github/retrooper/packetevents/util/LogManager.java
+++ api/src/main/java/com/github/retrooper/packetevents/util/LogManager.java
@@ -2,30 +2,31 @@ package com.github.retrooper.packetevents.util;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.protocol.chat.Color;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.logging.Level;
 
 public class LogManager {
 
-    protected void log(Level level, Color color, String message) {
-        PacketEvents.getAPI().getLogger().log(level, color.toString() + message);
+    protected void log(Level level, @Nullable Color color, String message) {
+        PacketEvents.getAPI().getLogger().log(level, color != null ? (color.toString()) : "" + message);
     }
 
     public void info(String message) {
-        log(Level.INFO, Color.BRIGHT_GREEN, message);
+        log(Level.INFO, null, message);
     }
 
     public void warn(final String message) {
-        log(Level.WARNING, Color.YELLOW, message);
+        log(Level.WARNING, null, message);
     }
 
-    public void severe(final String message) {
-        log(Level.SEVERE, Color.RED, message);
+    public void severe(String message) {
+        log(Level.SEVERE, null, message);
     }
 
-    public void debug(final String message) {
+    public void debug(String message) {
         if (PacketEvents.getAPI().getSettings().isDebugEnabled()) {
-            log(Level.FINE, Color.GRAY, message);
+            log(Level.FINE, null, message);
         }
     }
 }
