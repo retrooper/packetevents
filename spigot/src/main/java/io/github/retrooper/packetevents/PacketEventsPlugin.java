e77d23e908f5ae7e2460e4026b430900630251ed
retrooper <retrooperdev@gmail.com>
2022-01-01 21:59:17 +0100

    Removed some things to prevent compilation

--- spigot/src/main/java/io/github/retrooper/packetevents/PacketEventsPlugin.java
+++ spigot/src/main/java/io/github/retrooper/packetevents/PacketEventsPlugin.java
@@ -123,8 +123,6 @@ public class PacketEventsPlugin extends JavaPlugin {
             }
         };
         PacketEvents.getAPI().getEventManager().registerListener(debugListener);
-        net.minecraft.server.v1_10_R1.PacketPlayOutEntityMetadata em0;
-        net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata em;
     }
 
     @Override
