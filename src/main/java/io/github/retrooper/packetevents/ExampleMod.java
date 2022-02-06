74e615ec40d13f7e71d47ffb68617021b27f3e5d
retrooper <retrooperdev@gmail.com>
2022-02-06 18:44:12 +0100

    Cleaned stuff up, made a FabricPacketEventsBuilder

--- src/main/java/io/github/retrooper/packetevents/ExampleMod.java
+++ src/main/java/io/github/retrooper/packetevents/ExampleMod.java
@@ -4,29 +4,14 @@ import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
-import com.github.retrooper.packetevents.injector.ChannelInjector;
-import com.github.retrooper.packetevents.manager.server.ServerVersion;
-import com.github.retrooper.packetevents.netty.channel.ChannelAbstract;
 import com.github.retrooper.packetevents.protocol.ConnectionState;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
-import com.github.retrooper.packetevents.protocol.player.User;
-import com.github.retrooper.packetevents.util.reflection.ReflectionObject;
 import com.github.retrooper.packetevents.wrapper.handshaking.client.WrapperHandshakingClientHandshake;
-import io.github.retrooper.packetevents.factory.netty.BuildData;
-import io.github.retrooper.packetevents.factory.netty.NettyPacketEventsBuilder;
-import io.github.retrooper.packetevents.handler.PacketDecoder;
-import io.github.retrooper.packetevents.handler.PacketEncoder;
-import io.github.retrooper.packetevents.manager.player.PlayerManagerImpl;
-import io.github.retrooper.packetevents.manager.server.ServerManagerImpl;
-import io.netty.channel.Channel;
+import io.github.retrooper.packetevents.factory.fabric.FabricPacketEventsBuilder;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.util.ActionResult;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,82 +28,7 @@ public class ExampleMod implements ModInitializer {
         // Proceed with mild caution.
         //TODO Mod idea, every 30 seconds or so, on a new port, establish a new connection to the server, get a server list response and
         //that is your ping
-        BuildData data = new BuildData("fabric");
-        ChannelInjector injector = new ChannelInjector() {
-            @Override
-            public @Nullable ConnectionState getConnectionState(ChannelAbstract ch) {
-                Channel channel = (Channel) ch.rawChannel();
-                PacketDecoder decoder = (PacketDecoder) channel.pipeline().get(PacketEvents.DECODER_NAME);
-                return decoder.user.getConnectionState();
-            }
-
-            @Override
-            public void changeConnectionState(ChannelAbstract ch, @Nullable ConnectionState packetState) {
-                Channel channel = (Channel) ch.rawChannel();
-                PacketDecoder decoder = (PacketDecoder) channel.pipeline().get(PacketEvents.DECODER_NAME);
-                decoder.user.setConnectionState(packetState);
-            }
-
-            @Override
-            public void inject() {
-
-            }
-
-            @Override
-            public void eject() {
-
-            }
-
-            @Override
-            public void injectPlayer(Object player, @Nullable ConnectionState connectionState) {
-
-            }
-
-            @Override
-            public void updateUser(ChannelAbstract ch, User user) {
-                Channel channel = (Channel) ch.rawChannel();
-                PacketDecoder decoder = (PacketDecoder) channel.pipeline().get(PacketEvents.DECODER_NAME);
-                decoder.user = user;
-                PacketEncoder encoder = (PacketEncoder) channel.pipeline().get(PacketEvents.ENCODER_NAME);
-                encoder.user = user;
-            }
-
-            @Override
-            public void ejectPlayer(Object player) {
-
-            }
-
-            @Override
-            public boolean hasInjected(Object player) {
-                return false;
-            }
-        };
-
-        ServerManagerImpl serverManager = new ServerManagerImpl() {
-            @Override
-            public ServerVersion getVersion() {
-                return ServerVersion.getLatest();
-            }
-        };
-
-        PlayerManagerImpl playerManager = new PlayerManagerImpl() {
-            @Override
-            public int getPing(@NotNull Object player) {
-                return 0;
-            }
-
-            @Override
-            public ChannelAbstract getChannel(@NotNull Object player) {
-                ClientConnection connection = ((ClientPlayerEntity) player).networkHandler.getConnection();
-                ReflectionObject reflectConnection = new ReflectionObject(connection);
-                Channel channel = reflectConnection.readObject(0, Channel.class);
-                return PacketEvents.getAPI().getNettyManager().wrapChannel(channel);
-            }
-        };
-
-        PacketEvents.setAPI(NettyPacketEventsBuilder.build(data, injector,
-                serverManager, playerManager));
-
+        PacketEvents.setAPI(FabricPacketEventsBuilder.build("modid"));
         PacketEvents.getAPI().getSettings().debug(true).bStats(true);
         PacketEvents.getAPI().load();
         PacketEvents.getAPI().getEventManager().registerListener(new PacketListenerAbstract() {
@@ -130,8 +40,10 @@ public class ExampleMod implements ModInitializer {
                     String address = handshake.getServerAddress();
                     int port = handshake.getServerPort();
                     System.out.println("Sent handshaking packet: " + version + " " + address + ":" + port);
+                } else if (event.getConnectionState() == ConnectionState.PLAY) {
+                    ClientPlayerEntity player = (ClientPlayerEntity) event.getPlayer();
+                    MinecraftClient.getInstance().currentScreen.sendMessage("Packet sent: " + event.getPacketType());
                 }
-                System.out.println("Packet type: " + event.getPacketType().getName());
             }
 
             @Override
@@ -140,13 +52,7 @@ public class ExampleMod implements ModInitializer {
             }
         });
         PacketEvents.getAPI().init();
-
-
-        LOGGER.info("Hello Fabric world!");
-        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) ->
-        {
-            LOGGER.info("ez");
-            return ActionResult.PASS;
-        });
     }
+
+
 }
---
+++ src/main/java/io/github/retrooper/packetevents/factory/fabric/FabricPacketEventsBuilder.java
@@ -0,0 +1,244 @@
+package io.github.retrooper.packetevents.factory.fabric;
+
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.PacketEventsAPI;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import com.github.retrooper.packetevents.injector.ChannelInjector;
+import com.github.retrooper.packetevents.injector.InternalPacketListener;
+import com.github.retrooper.packetevents.manager.player.PlayerManager;
+import com.github.retrooper.packetevents.manager.server.ServerManager;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
+import com.github.retrooper.packetevents.netty.NettyManager;
+import com.github.retrooper.packetevents.netty.channel.ChannelAbstract;
+import com.github.retrooper.packetevents.protocol.ConnectionState;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.User;
+import com.github.retrooper.packetevents.settings.PacketEventsSettings;
+import com.github.retrooper.packetevents.util.reflection.ReflectionObject;
+import io.github.retrooper.packetevents.handler.PacketDecoder;
+import io.github.retrooper.packetevents.handler.PacketEncoder;
+import io.github.retrooper.packetevents.manager.netty.NettyManagerImpl;
+import io.github.retrooper.packetevents.manager.player.PlayerManagerImpl;
+import io.github.retrooper.packetevents.manager.server.ServerManagerImpl;
+import io.netty.channel.Channel;
+import net.minecraft.SharedConstants;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.ClientConnection;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FabricPacketEventsBuilder {
+    private static PacketEventsAPI<MinecraftClient> INSTANCE;
+
+    public static void clearBuildCache() {
+        INSTANCE = null;
+    }
+
+    public static PacketEventsAPI<MinecraftClient> build(String modId) {
+        if (INSTANCE == null) {
+            INSTANCE = buildNoCache(modId);
+        }
+        return INSTANCE;
+    }
+
+    public static PacketEventsAPI<MinecraftClient> build(String modId, PacketEventsSettings settings) {
+        if (INSTANCE == null) {
+            INSTANCE = buildNoCache(modId, settings);
+        }
+        return INSTANCE;
+    }
+
+
+    public static PacketEventsAPI<MinecraftClient> buildNoCache(String modId) {
+        return buildNoCache(modId, new PacketEventsSettings());
+    }
+
+    public static PacketEventsAPI<MinecraftClient> buildNoCache(String modId, PacketEventsSettings inSettings) {
+        return new PacketEventsAPI<>() {
+            private final PacketEventsSettings settings = inSettings;
+            private final ServerManager serverManager = new ServerManagerImpl() {
+                private static ServerVersion VERSION;
+
+                @Override
+                public ServerVersion getVersion() {
+                    //TODO Not perfect, as this is on the client! Might be inaccurate by a few patch versions.
+                    if (VERSION == null) {
+                        int targetPV = SharedConstants.getProtocolVersion();
+                        for (ServerVersion version : ServerVersion.reversedValues()) {
+                            if (version.getProtocolVersion() == targetPV) {
+                                VERSION = version;
+                            }
+                        }
+                    }
+                    return VERSION;
+                }
+            };
+
+            private final PlayerManagerImpl playerManager = new PlayerManagerImpl() {
+                @Override
+                public int getPing(@NotNull Object player) {
+                    return 0;
+                }
+
+                @Override
+                public ChannelAbstract getChannel(@NotNull Object player) {
+                    ClientConnection connection = ((ClientPlayerEntity) player).networkHandler.getConnection();
+                    ReflectionObject reflectConnection = new ReflectionObject(connection);
+                    Channel channel = reflectConnection.readObject(0, Channel.class);
+                    return PacketEvents.getAPI().getNettyManager().wrapChannel(channel);
+                }
+            };
+
+            private final ChannelInjector injector = new ChannelInjector() {
+                @Override
+                public @Nullable ConnectionState getConnectionState(ChannelAbstract ch) {
+                    Channel channel = (Channel) ch.rawChannel();
+                    PacketDecoder decoder = (PacketDecoder) channel.pipeline().get(PacketEvents.DECODER_NAME);
+                    return decoder.user.getConnectionState();
+                }
+
+                @Override
+                public void changeConnectionState(ChannelAbstract ch, @Nullable ConnectionState packetState) {
+                    Channel channel = (Channel) ch.rawChannel();
+                    PacketDecoder decoder = (PacketDecoder) channel.pipeline().get(PacketEvents.DECODER_NAME);
+                    decoder.user.setConnectionState(packetState);
+                }
+
+                @Override
+                public void inject() {
+
+                }
+
+                @Override
+                public void eject() {
+
+                }
+
+                @Override
+                public void injectPlayer(Object player, @Nullable ConnectionState connectionState) {
+
+                }
+
+                @Override
+                public void updateUser(ChannelAbstract ch, User user) {
+                    Channel channel = (Channel) ch.rawChannel();
+                    PacketDecoder decoder = (PacketDecoder) channel.pipeline().get(PacketEvents.DECODER_NAME);
+                    decoder.user = user;
+                    PacketEncoder encoder = (PacketEncoder) channel.pipeline().get(PacketEvents.ENCODER_NAME);
+                    encoder.user = user;
+                }
+
+                @Override
+                public void ejectPlayer(Object player) {
+
+                }
+
+                @Override
+                public boolean hasInjected(Object player) {
+                    return false;
+                }
+            };
+            private final NettyManager nettyManager = new NettyManagerImpl();
+            private boolean loaded;
+            private boolean initialized;
+            private boolean lateBind = false;
+
+            @Override
+            public void load() {
+                if (!loaded) {
+                    final String id = modId.toLowerCase();
+                    //Resolve server version and cache
+                    PacketEvents.IDENTIFIER = "pe-" + id;
+                    PacketEvents.ENCODER_NAME = "pe-encoder-" + id;
+                    PacketEvents.DECODER_NAME = "pe-decoder-" + id;
+                    PacketEvents.CONNECTION_NAME = "pe-connection-handler-" + id;
+                    PacketEvents.SERVER_CHANNEL_HANDLER_NAME = "pe-connection-initializer-" + id;
+
+                    lateBind = !injector.isBound();
+                    //If late-bind is enabled, we will inject a bit later.
+                    if (!lateBind) {
+                        injector.inject();
+                    }
+
+                    loaded = true;
+
+                    //Register internal packet listener (should be the first listener)
+                    //This listener doesn't do any modifications to the packets, just reads data
+                    getEventManager().registerListener(new InternalPacketListener(),
+                            PacketListenerPriority.LOWEST, true);
+                }
+            }
+
+            @Override
+            public boolean isLoaded() {
+                return loaded;
+            }
+
+            @Override
+            public void init() {
+                //Load if we haven't loaded already
+                load();
+                if (!initialized) {
+                    if (settings.shouldCheckForUpdates()) {
+                        getUpdateChecker().handleUpdateCheck();
+                    }
+
+                    if (settings.isbStatsEnabled()) {
+                        //TODO Cross-platform metrics?
+                    }
+
+                    PacketType.Play.Client.load();
+                    PacketType.Play.Server.load();
+                    initialized = true;
+                }
+            }
+
+            @Override
+            public boolean isInitialized() {
+                return initialized;
+            }
+
+            @Override
+            public void terminate() {
+                if (initialized) {
+                    //Eject the injector if needed(depends on the injector implementation)
+                    injector.eject();
+                    //Unregister all our listeners
+                    getEventManager().unregisterAllListeners();
+                    initialized = false;
+                }
+            }
+
+            @Override
+            public MinecraftClient getPlugin() {
+                return MinecraftClient.getInstance();
+            }
+
+            @Override
+            public ServerManager getServerManager() {
+                return serverManager;
+            }
+
+            @Override
+            public PlayerManager getPlayerManager() {
+                return playerManager;
+            }
+
+            @Override
+            public ChannelInjector getInjector() {
+                return injector;
+            }
+
+            @Override
+            public PacketEventsSettings getSettings() {
+                return settings;
+            }
+
+            @Override
+            public NettyManager getNettyManager() {
+                return nettyManager;
+            }
+        };
+    }
+}
