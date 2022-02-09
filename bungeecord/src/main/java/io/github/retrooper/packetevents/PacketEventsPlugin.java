215ef2a20d4e3baa38a30f4abec5c0ceabad6fe0
retrooper <retrooperdev@gmail.com>
2022-02-09 17:55:37 +0100

    Working on BungeeCord support.

---
+++ bungeecord/src/main/java/io/github/retrooper/packetevents/PacketEventsPlugin.java
@@ -0,0 +1,16 @@
+package io.github.retrooper.packetevents;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class PacketEventsPlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
