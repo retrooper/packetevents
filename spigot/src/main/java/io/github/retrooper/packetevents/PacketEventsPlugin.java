9a86f800c7c5ebf3edf817bd3f1fa1dabe632d25
retrooper <retrooperdev@gmail.com>
2022-03-10 22:05:04 +0100

    No debugs

--- spigot/src/main/java/io/github/retrooper/packetevents/PacketEventsPlugin.java
+++ spigot/src/main/java/io/github/retrooper/packetevents/PacketEventsPlugin.java
@@ -146,7 +146,7 @@ public class PacketEventsPlugin extends JavaPlugin {
                 }
             }
         };
-        PacketEvents.getAPI().getEventManager().registerListener(listener);
+        //PacketEvents.getAPI().getEventManager().registerListener(listener);
     }
 
     @Override
