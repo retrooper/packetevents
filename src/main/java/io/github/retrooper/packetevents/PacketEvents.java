177e9b7f4ef86c5382c4ef82523aff06c7a8667b
retrooper <retrooperdev@gmail.com>
2022-03-24 20:07:23 +0100

    Releasing 1.8.3

--- src/main/java/io/github/retrooper/packetevents/PacketEvents.java
+++ src/main/java/io/github/retrooper/packetevents/PacketEvents.java
@@ -57,7 +57,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public final class PacketEvents implements Listener, EventManager {
     private static PacketEvents instance;
     private static Plugin plugin;
-    private final PEVersion version = new PEVersion(1, 8, 2);
+    private final PEVersion version = new PEVersion(1, 8, 3);
     private final EventManager eventManager = new PEEventManager();
     private final PlayerUtils playerUtils = new PlayerUtils();
     private final ServerUtils serverUtils = new ServerUtils();
