e9f0e1d7afe32a51f954ab488ec7226de684218b
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-04-11 23:26:13 -0500

    Fix WrapperPlayServerSpawnPlayer

--- api/src/main/java/com/github/retrooper/packetevents/wrapper/play/server/WrapperPlayServerSpawnPlayer.java
+++ api/src/main/java/com/github/retrooper/packetevents/wrapper/play/server/WrapperPlayServerSpawnPlayer.java
@@ -25,7 +25,6 @@ import com.github.retrooper.packetevents.protocol.item.type.ItemType;
 import com.github.retrooper.packetevents.protocol.item.type.ItemTypes;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.world.Location;
-import com.github.retrooper.packetevents.util.MathUtil;
 import com.github.retrooper.packetevents.util.Vector3d;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 
@@ -73,25 +72,20 @@ public class WrapperPlayServerSpawnPlayer extends PacketWrapper<WrapperPlayServe
     public void read() {
         entityID = readVarInt();
         uuid = readUUID();
-        boolean v1_9 = serverVersion.isNewerThanOrEquals(ServerVersion.V_1_9);
-        if (v1_9) {
-            position = new Vector3d(readDouble(), readDouble(), readDouble());
-        }
-        else {
-            position = new Vector3d(readInt() / 32.0, readInt() / 32.0, readInt() / 32.0);
-        }
-        yaw = readByte() /  ROTATION_DIVISOR;
+        position = new Vector3d(readDouble(), readDouble(), readDouble());
+
+        yaw = readByte() / ROTATION_DIVISOR;
         pitch = readByte() / ROTATION_DIVISOR;
+
+        boolean v1_9 = serverVersion.isNewerThanOrEquals(ServerVersion.V_1_9);
         if (!v1_9) {
             item = ItemTypes.getById(serverVersion.toClientVersion(), readByte());
-        }
-        else {
+        } else {
             item = ItemTypes.AIR;
         }
         if (serverVersion.isOlderThan(ServerVersion.V_1_15)) {
             entityMetadata = readEntityMetadata();
-        }
-        else {
+        } else {
             entityMetadata = new ArrayList<>();
         }
     }
@@ -109,19 +103,15 @@ public class WrapperPlayServerSpawnPlayer extends PacketWrapper<WrapperPlayServe
     public void write() {
         writeVarInt(entityID);
         writeUUID(uuid);
-        boolean v1_9 = serverVersion.isNewerThanOrEquals(ServerVersion.V_1_9);
-        if (v1_9) {
-            writeDouble(position.getX());
-            writeDouble(position.getY());
-            writeDouble(position.getZ());
-        }
-        else {
-            writeInt(MathUtil.floor(position.getX() * 32.0));
-            writeInt(MathUtil.floor(position.getY() * 32.0));
-            writeInt(MathUtil.floor(position.getZ() * 32.0));
-        }
+
+        writeDouble(position.getX());
+        writeDouble(position.getY());
+        writeDouble(position.getZ());
+
         writeByte((byte) (yaw * ROTATION_DIVISOR));
         writeByte((byte) (pitch * ROTATION_DIVISOR));
+
+        boolean v1_9 = serverVersion.isNewerThanOrEquals(ServerVersion.V_1_9);
         if (!v1_9) {
             writeShort(item.getId(serverVersion.toClientVersion()));
         }
