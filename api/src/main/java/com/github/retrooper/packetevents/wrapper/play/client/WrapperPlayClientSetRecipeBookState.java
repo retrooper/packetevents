8224019e23413cc1a42f13bd6af4ba59ffc36c6a
NoJokeFNA <NoJokeFNA@gmail.com>
2022-05-12 12:42:01 +0200

    change: code-style

--- api/src/main/java/com/github/retrooper/packetevents/wrapper/play/client/WrapperPlayClientSetRecipeBookState.java
+++ api/src/main/java/com/github/retrooper/packetevents/wrapper/play/client/WrapperPlayClientSetRecipeBookState.java
@@ -24,63 +24,63 @@ import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 
 public class WrapperPlayClientSetRecipeBookState extends PacketWrapper<WrapperPlayClientSetRecipeBookState> {
-    private BookType bookType;
-    private boolean bookOpen;
-    private boolean filterActive;
+  private BookType bookType;
+  private boolean bookOpen;
+  private boolean filterActive;
 
-    public WrapperPlayClientSetRecipeBookState(PacketReceiveEvent event) {
-        super(event);
-    }
+  public WrapperPlayClientSetRecipeBookState(PacketReceiveEvent event) {
+    super(event);
+  }
 
-    public WrapperPlayClientSetRecipeBookState(BookType bookType, boolean bookOpen, boolean filterActive) {
-        super(PacketType.Play.Client.SET_RECIPE_BOOK_STATE);
-        this.bookType = bookType;
-        this.bookOpen = bookOpen;
-        this.filterActive = filterActive;
-    }
+  public WrapperPlayClientSetRecipeBookState(BookType bookType, boolean bookOpen, boolean filterActive) {
+    super(PacketType.Play.Client.SET_RECIPE_BOOK_STATE);
+    this.bookType = bookType;
+    this.bookOpen = bookOpen;
+    this.filterActive = filterActive;
+  }
 
-    @Override
-    public void read() {
-        this.bookType = BookType.getById(readVarInt());
-        this.bookOpen = readBoolean();
-        this.filterActive = readBoolean();
-    }
+  @Override
+  public void read() {
+    this.bookType = BookType.getById(readVarInt());
+    this.bookOpen = readBoolean();
+    this.filterActive = readBoolean();
+  }
 
-    @Override
-    public void write() {
-        writeVarInt(this.bookType.getId());
-        writeBoolean(this.bookOpen);
-        writeBoolean(this.filterActive);
-    }
+  @Override
+  public void write() {
+    writeVarInt(this.bookType.getId());
+    writeBoolean(this.bookOpen);
+    writeBoolean(this.filterActive);
+  }
 
-    @Override
-    public void copy(WrapperPlayClientSetRecipeBookState wrapper) {
-        this.bookType = wrapper.bookType;
-        this.bookOpen = wrapper.bookOpen;
-        this.filterActive = wrapper.filterActive;
-    }
+  @Override
+  public void copy(WrapperPlayClientSetRecipeBookState wrapper) {
+    this.bookType = wrapper.bookType;
+    this.bookOpen = wrapper.bookOpen;
+    this.filterActive = wrapper.filterActive;
+  }
 
-    public BookType getBookType() {
-        return bookType;
-    }
+  public BookType getBookType() {
+    return bookType;
+  }
 
-    public void setBookType(BookType bookType) {
-        this.bookType = bookType;
-    }
+  public void setBookType(BookType bookType) {
+    this.bookType = bookType;
+  }
 
-    public boolean isBookOpen() {
-        return bookOpen;
-    }
+  public boolean isBookOpen() {
+    return bookOpen;
+  }
 
-    public void setBookOpen(boolean bookOpen) {
-        this.bookOpen = bookOpen;
-    }
+  public void setBookOpen(boolean bookOpen) {
+    this.bookOpen = bookOpen;
+  }
 
-    public boolean isFilterActive() {
-        return filterActive;
-    }
+  public boolean isFilterActive() {
+    return filterActive;
+  }
 
-    public void setFilterActive(boolean filterActive) {
-        this.filterActive = filterActive;
-    }
+  public void setFilterActive(boolean filterActive) {
+    this.filterActive = filterActive;
+  }
 }
