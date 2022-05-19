b5590f2a664b6912cb085821f7dc573273f9bc62
NoJokeFNA <nojokefna@gmail.com>
2022-05-19 18:14:35 +0200

    fix: fixed code-style

--- api/src/main/java/com/github/retrooper/packetevents/protocol/recipe/RecipeState.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/recipe/RecipeState.java
@@ -19,8 +19,8 @@
 package com.github.retrooper.packetevents.protocol.recipe;
 
 public enum RecipeState {
-  DISPLAYED_RECIPE,
-  RECIPE_BOOK_STATE;
+    DISPLAYED_RECIPE,
+    RECIPE_BOOK_STATE;
 
-  public static final RecipeState[] VALUES = values();
+    public static final RecipeState[] VALUES = values();
 }
