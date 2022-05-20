6c3d6f25ac0683c8435ceb9bf767582d0689c1a0
NoJokeFNA <nojokefna@gmail.com>
2022-05-20 14:48:32 +0200

    fix: code-style

--- api/src/main/java/com/github/retrooper/packetevents/protocol/world/Direction.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/world/Direction.java
@@ -2,27 +2,27 @@ package com.github.retrooper.packetevents.protocol.world;
 
 // From MCProtocolLib
 public enum Direction {
-  DOWN(-1),
-  UP(-1),
-  NORTH(0),
-  SOUTH(1),
-  WEST(2),
-  EAST(3);
+    DOWN(-1),
+    UP(-1),
+    NORTH(0),
+    SOUTH(1),
+    WEST(2),
+    EAST(3);
 
-  private final int horizontalIndex;
+    private final int horizontalIndex;
 
-  Direction(int horizontalIndex) {
-    this.horizontalIndex = horizontalIndex;
-  }
+    Direction(int horizontalIndex) {
+        this.horizontalIndex = horizontalIndex;
+    }
 
-  public int getHorizontalIndex() {
-    return horizontalIndex;
-  }
+    public int getHorizontalIndex() {
+        return horizontalIndex;
+    }
 
-  private static final Direction[] HORIZONTAL_VALUES = {NORTH, SOUTH, WEST, EAST};
-  public static final Direction[] VALUES = values();
+    private static final Direction[] HORIZONTAL_VALUES = {NORTH, SOUTH, WEST, EAST};
+    public static final Direction[] VALUES = values();
 
-  public static Direction getByHorizontalIndex(int index) {
-    return HORIZONTAL_VALUES[index % HORIZONTAL_VALUES.length];
-  }
+    public static Direction getByHorizontalIndex(int index) {
+        return HORIZONTAL_VALUES[index % HORIZONTAL_VALUES.length];
+    }
 }
