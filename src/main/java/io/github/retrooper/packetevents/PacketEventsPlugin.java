5e42bef4883d39b5f316bf0563bf7bb6d081a31c
retrooper <retrooperdev@gmail.com>
2022-06-20 05:49:17 +0200

    Minor changes to Main class

--- src/main/java/io/github/retrooper/packetevents/PacketEventsPlugin.java
+++ src/main/java/io/github/retrooper/packetevents/PacketEventsPlugin.java
@@ -21,8 +21,11 @@ package io.github.retrooper.packetevents;
 import io.github.retrooper.packetevents.event.PacketListenerAbstract;
 import io.github.retrooper.packetevents.event.impl.PacketLoginReceiveEvent;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.event.impl.PacketPlaySendEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
 import io.github.retrooper.packetevents.packetwrappers.login.in.encryptionbegin.WrappedPacketLoginInEncryptionBegin;
+import io.github.retrooper.packetevents.packetwrappers.play.out.entity.WrappedPacketOutEntity;
+import io.github.retrooper.packetevents.packetwrappers.play.out.entityeffect.WrappedPacketOutEntityEffect;
 import io.github.retrooper.packetevents.packetwrappers.play.out.setslot.WrappedPacketOutSetSlot;
 import io.github.retrooper.packetevents.settings.PacketEventsSettings;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
@@ -54,6 +57,20 @@ public class PacketEventsPlugin extends JavaPlugin {
                     ItemStack stack = new ItemStack(Material.STICK);
                     WrappedPacketOutSetSlot setSlot = new WrappedPacketOutSetSlot(0, 37, stack);
                     PacketEvents.get().getPlayerUtils().sendPacket(event.getPlayer(), setSlot);
+                }
+            }
+
+            @Override
+            public void onPacketPlaySend(PacketPlaySendEvent event) {
+                if (PacketType.Play.Server.Util.isInstanceOfEntity(event.getPacketId())) {
+                    WrappedPacketOutEntity entity = new WrappedPacketOutEntity(event.getNMSPacket());
+                    System.out.println("en: " + entity.getEntityId() + ", delta x: " + entity.getDeltaX()
+                            + ", delta y: " + entity.getDeltaY() +  ", delta z: " + entity.getDeltaZ());
+                }
+                else if (event.getPacketId() == PacketType.Play.Server.ENTITY_EFFECT) {
+                    WrappedPacketOutEntityEffect eff = new WrappedPacketOutEntityEffect(event.getNMSPacket());
+                    System.out.println("eff: " + eff.getEffectId() + ", ampl: " + eff.getAmplifier() + ", dur:" + eff.getDuration());
+
                 }
             }
 
