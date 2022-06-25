1ec72afb60ceb2483fb200fcf947a1a9216503a8
scarfacered <98234136+scarfacered@users.noreply.github.com>
2022-06-25 00:03:45 -0500

    I said 1500 but did 1300...

--- api/src/main/java/com/github/retrooper/packetevents/wrapper/handshaking/client/WrapperHandshakingClientHandshake.java
+++ api/src/main/java/com/github/retrooper/packetevents/wrapper/handshaking/client/WrapperHandshakingClientHandshake.java
@@ -52,7 +52,7 @@ public class WrapperHandshakingClientHandshake extends PacketWrapper<WrapperHand
     public void read() {
         this.protocolVersion = readVarInt();
         this.clientVersion = ClientVersion.getById(protocolVersion);
-        this.serverAddress = readString(1300); // Velocity's legacy player-info-forwarding-mode increases this past default value of 255.
+        this.serverAddress = readString(1500); // Velocity's legacy player-info-forwarding-mode increases this past default value of 255.
         this.serverPort = readUnsignedShort();
         int nextStateIndex = readVarInt();
         this.nextConnectionState = ConnectionState.getById(nextStateIndex);
@@ -61,7 +61,7 @@ public class WrapperHandshakingClientHandshake extends PacketWrapper<WrapperHand
     @Override
     public void write() {
         writeVarInt(protocolVersion);
-        writeString(serverAddress, 1300); // Velocity's legacy player-info-forwarding-mode increases this past default value of 255.
+        writeString(serverAddress, 1500); // Velocity's legacy player-info-forwarding-mode increases this past default value of 255.
         writeShort(serverPort);
         writeVarInt(nextConnectionState.ordinal());
     }
