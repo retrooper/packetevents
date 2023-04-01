3f00e1d817b40d63e56ee1bfc1fe1b2da05061ce
MWHunter <s0521458@student.rockvalleycollege.edu>
2023-04-01 14:50:47 -0500

    more folia support utils

--- spigot/src/main/java/io/github/retrooper/packetevents/util/FoliaCompatUtil.java
+++ spigot/src/main/java/io/github/retrooper/packetevents/util/FoliaCompatUtil.java
@@ -2,6 +2,7 @@ package io.github.retrooper.packetevents.util;
 
 import com.github.retrooper.packetevents.util.reflection.Reflection;
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
@@ -10,6 +11,7 @@ import org.bukkit.plugin.Plugin;
 
 import java.lang.reflect.Method;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 public class FoliaCompatUtil {
     // Thanks powercas_gamer in #folia-dev
@@ -23,6 +25,11 @@ public class FoliaCompatUtil {
     }
     private static final boolean folia = isFolia();
 
+    /**
+     * Run a task async, either with bukkit scheduler or using java
+     * @param plugin Your plugin or PacketEvents
+     * @param run Runnable to run
+     */
     public static void runTaskAsync(Plugin plugin, Runnable run) {
         if (!folia) {
             Bukkit.getScheduler().runTaskAsynchronously(plugin, run);
@@ -31,6 +38,37 @@ public class FoliaCompatUtil {
         CompletableFuture.runAsync(run);
     }
 
+    /**
+     * Run a task every global tick
+     * @param plugin Your plugin or PacketEvents
+     * @param run Consumer that accepts an object or null, for Folia or Paper/Spigot respectively
+     * @param delay Delay in ticks
+     * @param period Period in ticks
+     */
+    public static void runTaskTimerAsync(Plugin plugin, Consumer<Object> run, long delay, long period) {
+        if (!folia) {
+            Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> run.accept(null), delay, period);
+            return;
+        }
+        try {
+            Method getSchedulerMethod = Reflection.getMethod(Server.class, "getGlobalRegionScheduler", 0);
+            Object globalRegionScheduler = getSchedulerMethod.invoke(Bukkit.getServer());
+
+            Class<?> schedulerClass = globalRegionScheduler.getClass();
+            Method executeMethod = schedulerClass.getMethod("runAtFixedRate", Plugin.class, Consumer.class, long.class, long.class);
+
+            executeMethod.invoke(globalRegionScheduler, plugin, run, delay, period);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Run a task after the server has finished initializing.
+     * Undefined behavior if called after the server has finished initializing.
+     * @param plugin Your plugin or PacketEvents
+     * @param run The task to run
+     */
     public static void runTaskOnInit(Plugin plugin, Runnable run) {
         if (!folia) {
             Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, run);
@@ -50,6 +88,14 @@ public class FoliaCompatUtil {
         }, EventPriority.HIGHEST, (listener, event) -> run.run(), plugin);
     }
 
+    /**
+     * Run a task for an entity on whatever thread the entity is on
+     * @param entity The entity to run the task for
+     * @param plugin Your plugin or PacketEvents
+     * @param run The task to run
+     * @param retired The task to run if entity is retired before the task is run
+     * @param delay Delay in ticks
+     */
     public static void runTaskForEntity(Entity entity, Plugin plugin, Runnable run, Runnable retired, long delay) {
         if (!folia) {
             Bukkit.getScheduler().runTaskLater(plugin, run, delay);
