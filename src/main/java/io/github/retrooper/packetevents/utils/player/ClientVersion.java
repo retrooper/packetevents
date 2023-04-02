3581e23923e939c50109b2bd54970a76d4df5411
retrooper <retrooperdev@gmail.com>
2023-04-02 17:25:52 +0200

    Minimal 1.19.4 update

--- src/main/java/io/github/retrooper/packetevents/utils/player/ClientVersion.java
+++ src/main/java/io/github/retrooper/packetevents/utils/player/ClientVersion.java
@@ -94,12 +94,12 @@ public enum ClientVersion {
 
     v_1_18_2(758),
 
-    v_1_19(759), v_1_19_1(760), v_1_19_3(761),
+    v_1_19(759), v_1_19_1(760), v_1_19_3(761), v_1_19_4(762),
     //TODO Update(checkpoint for things to look out for when updating)
 
     LOWER_THAN_SUPPORTED_VERSIONS(v_1_7_10.protocolVersion - 1),
     //TODO Update(checkpoint for things to look out for when updating)
-    HIGHER_THAN_SUPPORTED_VERSIONS(v_1_19_3.protocolVersion + 1),
+    HIGHER_THAN_SUPPORTED_VERSIONS(v_1_19_4.protocolVersion + 1),
     /**
      * Pre releases just aren't supported, we would end up with so many enum constants.
      * This constant assures you they are on a pre-release.
@@ -120,7 +120,7 @@ public enum ClientVersion {
     //TODO Update(checkpoint for things to look out for when updating)
     private static final int[] CLIENT_VERSIONS = new int[]{5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338,
             340, 393, 401, 404, 477, 480, 485, 490, 498, 573,
-            575, 578, 735, 736, 751, 753, 754, 755, 756, 757, 758, 759, 760, 761};
+            575, 578, 735, 736, 751, 753, 754, 755, 756, 757, 758, 759, 760, 761, 762};
     private int protocolVersion;
 
     ClientVersion(int protocolVersion) {
