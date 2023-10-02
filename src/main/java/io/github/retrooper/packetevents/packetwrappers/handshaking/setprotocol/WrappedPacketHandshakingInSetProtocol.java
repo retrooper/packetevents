012bcae2d55734d5e61b016f0ff71b7554ef7da9
retrooper <retrooperdev@gmail.com>
2023-10-03 00:18:52 +0200

    Fix WrappedPacketHandshakingInSetProtocol implementation for 1.20.2

--- src/main/java/io/github/retrooper/packetevents/packetwrappers/handshaking/setprotocol/WrappedPacketHandshakingInSetProtocol.java
+++ src/main/java/io/github/retrooper/packetevents/packetwrappers/handshaking/setprotocol/WrappedPacketHandshakingInSetProtocol.java
@@ -23,30 +23,30 @@ import io.github.retrooper.packetevents.packetwrappers.WrappedPacket;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
 
 public class WrappedPacketHandshakingInSetProtocol extends WrappedPacket {
-    private static boolean v_1_17;
+    private static boolean between_V_1_17_And_1_20_1;
     public WrappedPacketHandshakingInSetProtocol(NMSPacket packet) {
         super(packet);
     }
 
     @Override
     protected void load() {
-        v_1_17 = version.isNewerThanOrEquals(ServerVersion.v_1_17);
+        between_V_1_17_And_1_20_1 = version.isNewerThanOrEquals(ServerVersion.v_1_17) && version.isOlderThan(ServerVersion.v_1_20_2);
     }
 
     public int getProtocolVersion() {
-        return readInt(v_1_17 ? 1 : 0);
+        return readInt(between_V_1_17_And_1_20_1 ? 1 : 0);
     }
 
     public void setProtocolVersion(int protocolVersion) {
-        writeInt(v_1_17 ? 1 : 0, protocolVersion);
+        writeInt(between_V_1_17_And_1_20_1 ? 1 : 0, protocolVersion);
     }
 
     public int getPort() {
-        return readInt(v_1_17 ? 2 : 1);
+        return readInt(between_V_1_17_And_1_20_1 ? 2 : 1);
     }
 
     public void setPort(int port) {
-        writeInt(v_1_17 ? 2 : 1, port);
+        writeInt(between_V_1_17_And_1_20_1 ? 2 : 1, port);
     }
 
     public String getHostName() {
