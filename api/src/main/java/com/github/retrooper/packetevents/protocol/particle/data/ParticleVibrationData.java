abea90564fed88dcfb8820089122e4fb25a33593
booky10 <boooky10@gmail.com>
2023-11-16 22:01:28 +0100

    Update vibration particle data

--- api/src/main/java/com/github/retrooper/packetevents/protocol/particle/data/ParticleVibrationData.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/particle/data/ParticleVibrationData.java
@@ -18,6 +18,10 @@
 
 package com.github.retrooper.packetevents.protocol.particle.data;
 
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
+import com.github.retrooper.packetevents.protocol.mapper.MappedEntity;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.github.retrooper.packetevents.resources.ResourceLocation;
 import com.github.retrooper.packetevents.util.Vector3i;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 import org.jetbrains.annotations.Nullable;
@@ -25,30 +29,54 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Optional;
 
 public class ParticleVibrationData extends ParticleData {
-    public enum PositionType {
-        BLOCK("minecraft:block"), ENTITY("minecraft:entity");
 
-        private final String name;
+    public enum PositionType implements MappedEntity {
 
-        PositionType(String name) {
+        BLOCK(new ResourceLocation("minecraft:block")),
+        ENTITY(new ResourceLocation("minecraft:entity"));
+
+        private final ResourceLocation name;
+
+        PositionType(ResourceLocation name) {
             this.name = name;
         }
 
-        public String getName() {
+        @Override
+        public ResourceLocation getName() {
             return name;
         }
 
-        public static PositionType getByName(String name) {
-            for(PositionType type : values()) {
-                if(type.getName().equals(name)) {
+        public static PositionType getById(int id) {
+            switch (id) {
+                case 0x00:
+                    return BLOCK;
+                case 0x01:
+                    return ENTITY;
+                default:
+                    throw new IllegalArgumentException("Illegal position type id: " + id);
+            }
+        }
+
+        public static @Nullable PositionType getByName(String name) {
+            return getByName(new ResourceLocation(name));
+        }
+
+        public static @Nullable PositionType getByName(ResourceLocation name) {
+            for (PositionType type : values()) {
+                if (type.getName().equals(name)) {
                     return type;
                 }
             }
             return null;
         }
+
+        @Override
+        public int getId(ClientVersion version) {
+            return this.ordinal();
+        }
     }
 
-    private Vector3i startingPosition;
+    private Vector3i startingPosition; // Removed in 1.19.4
     private PositionType type;
     private @Nullable Vector3i blockPosition;
     private @Nullable Integer entityId;
@@ -107,28 +135,44 @@ public class ParticleVibrationData extends ParticleData {
     }
 
     public static ParticleVibrationData read(PacketWrapper<?> wrapper) {
-        Vector3i startingPos = wrapper.readBlockPosition();
-        String positionTypeName = wrapper.readString();
-        PositionType positionType = PositionType.getByName(positionTypeName);
-        if (positionType == PositionType.BLOCK) {
-            return new ParticleVibrationData(startingPos, wrapper.readBlockPosition(), wrapper.readVarInt());
+        Vector3i startingPos = wrapper.getServerVersion().isNewerThanOrEquals(ServerVersion.V_1_19_4)
+                ? Vector3i.zero() : wrapper.readBlockPosition();
+
+        PositionType positionType;
+        if (wrapper.getServerVersion().isNewerThanOrEquals(ServerVersion.V_1_20_3)) {
+            positionType = PositionType.getById(wrapper.readVarInt());
+        } else {
+            String positionTypeName = wrapper.readString();
+            positionType = PositionType.getByName(positionTypeName);
+            if (positionType == null) {
+                throw new IllegalArgumentException("Unknown position type: " + positionTypeName);
+            }
         }
-        else if (positionType == PositionType.ENTITY) {
-            return new ParticleVibrationData(startingPos, wrapper.readVarInt(), wrapper.readVarInt());
-        }
-        else {
-            throw new IllegalArgumentException("Unknown position type: " + positionTypeName);
+
+        switch (positionType) {
+            case BLOCK:
+                return new ParticleVibrationData(startingPos, wrapper.readBlockPosition(), wrapper.readVarInt());
+            case ENTITY:
+                return new ParticleVibrationData(startingPos, wrapper.readVarInt(), wrapper.readVarInt());
+            default:
+                throw new IllegalArgumentException("Illegal position type: " + positionType);
         }
     }
 
-
     public static void write(PacketWrapper<?> wrapper, ParticleVibrationData data) {
-        wrapper.writeBlockPosition(data.getStartingPosition());
-        wrapper.writeString(data.getType().getName());
+        if (wrapper.getServerVersion().isOlderThan(ServerVersion.V_1_19_4)) {
+            wrapper.writeBlockPosition(data.getStartingPosition());
+        }
+
+        if (wrapper.getServerVersion().isNewerThanOrEquals(ServerVersion.V_1_20_3)) {
+            wrapper.writeVarInt(data.getType().getId(wrapper.getServerVersion().toClientVersion()));
+        } else {
+            wrapper.writeIdentifier(data.getType().getName());
+        }
+
         if (data.getType() == PositionType.BLOCK) {
             wrapper.writeBlockPosition(data.getBlockPosition().get());
-        }
-        else if (data.getType() == PositionType.ENTITY) {
+        } else if (data.getType() == PositionType.ENTITY) {
             wrapper.writeVarInt(data.getEntityId().get());
         }
         wrapper.writeVarInt(data.getTicks());
