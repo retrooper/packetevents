df6a334a0070d81c9986f0a55f29174426198d32
Tofaa <82680183+Tofaa2@users.noreply.github.com>
2024-01-06 03:20:32 +0300

    implement color api

---
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/color/Color.java
@@ -0,0 +1,55 @@
+package com.github.retrooper.packetevents.protocol.color;
+
+import net.kyori.adventure.util.RGBLike;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public final class Color implements RGBLike {
+
+    private static final int BIT_MASK = 0xFF;
+
+    private final int red, green, blue;
+
+    public Color(@Range(from = 0L, to = 255L) int red, @Range(from = 0L, to = 255L) int green, @Range(from = 0L, to = 255L) int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public Color(int rgb) {
+        this((rgb >> 16) & BIT_MASK, (rgb >> 8) & BIT_MASK, rgb & BIT_MASK);
+    }
+
+    public @NotNull Color withRed(@Range(from = 0L, to = 255L) int red) {
+        return new Color(red, green, blue);
+    }
+
+    public @NotNull Color withGreen(@Range(from = 0L, to = 255L) int green) {
+        return new Color(red, green, blue);
+    }
+
+    public @NotNull Color withBlue(@Range(from = 0L, to = 255L) int blue) {
+        return new Color(red, green, blue);
+    }
+
+    public int asRGB() {
+        int rgb = red;
+        rgb = (rgb << 8) + green;
+        return (rgb << 8) + blue;
+    }
+
+    @Override
+    public @Range(from = 0L, to = 255L) int red() {
+        return red;
+    }
+
+    @Override
+    public @Range(from = 0L, to = 255L) int green() {
+        return green;
+    }
+
+    @Override
+    public @Range(from = 0L, to = 255L) int blue() {
+        return blue;
+    }
+}
