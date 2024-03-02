5a3a4f65070f7ab175b0330dd1318999824b0b38
ytnoos <aleytnoos12@gmail.com>
2024-03-02 11:11:11 +0100

    Support for ViaVersion dev builds

--- spigot/src/main/java/io/github/retrooper/packetevents/util/viaversion/ViaVersionUtil.java
+++ spigot/src/main/java/io/github/retrooper/packetevents/util/viaversion/ViaVersionUtil.java
@@ -21,6 +21,7 @@ package io.github.retrooper.packetevents.util.viaversion;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.util.reflection.Reflection;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import io.netty.channel.Channel;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -78,7 +79,8 @@ public class ViaVersionUtil {
             Object viaEncoder = ((Channel) user.getChannel()).pipeline().get("via-encoder");
             Object connection = Reflection.getField(viaEncoder.getClass(), "connection").get(viaEncoder);
             Object protocolInfo = Reflection.getField(connection.getClass(), "protocolInfo").get(connection);
-            return (int) Reflection.getField(protocolInfo.getClass(), "protocolVersion").get(protocolInfo);
+            Object protocolVersion = Reflection.getField(protocolInfo.getClass(), "protocolVersion").get(protocolInfo);
+            return protocolVersion instanceof Integer ? (int) protocolVersion : ((ProtocolVersion) protocolVersion).getVersion();
         } catch (Exception e) {
             PacketEvents.getAPI().getLogManager().warn("Unable to grab ViaVersion client version for player!");
             return -1;
