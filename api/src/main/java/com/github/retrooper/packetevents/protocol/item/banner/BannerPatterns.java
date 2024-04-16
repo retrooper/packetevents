f20e8432b11923c23c6cca249c32601925c57466
booky10 <boooky10@gmail.com>
2024-04-16 18:07:56 +0200

    Finish banner patterns registry

--- api/src/main/java/com/github/retrooper/packetevents/protocol/item/banner/BannerPatterns.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/item/banner/BannerPatterns.java
@@ -33,11 +33,15 @@ public class BannerPatterns {
     private static final TypesBuilder TYPES_BUILDER = new TypesBuilder("item/item_banner_pattern_mappings",
             ClientVersion.V_1_20_5);
 
+    public static BannerPattern define(String key) {
+        ResourceLocation assetId = ResourceLocation.minecraft(key);
+        String translationKey = "block.minecraft.banner." + key;
+        return define(key, assetId, translationKey);
+    }
+
     public static BannerPattern define(String key, ResourceLocation assetId, String translationKey) {
         TypesBuilderData data = TYPES_BUILDER.defineFromArray(key);
-        BannerPattern type = new BannerPattern() {
-            private final int[] ids = data.getData();
-
+        BannerPattern pattern = new BannerPattern() {
             @Override
             public ResourceLocation getAssetId() {
                 return assetId;
@@ -55,19 +59,11 @@ public class BannerPatterns {
 
             @Override
             public int getId(ClientVersion version) {
-                int index = TYPES_BUILDER.getDataIndex(version);
-                return this.ids[index];
+                return TYPES_BUILDER.getDataIndex(version);
             }
         };
-
-        PATTERN_TYPE_MAP.put(type.getName().toString(), type);
-        for (ClientVersion version : TYPES_BUILDER.getVersions()) {
-            int index = TYPES_BUILDER.getDataIndex(version);
-            Map<Integer, BannerPattern> idMap = PATTERN_TYPE_ID_MAP.computeIfAbsent(
-                    (byte) index, k -> new HashMap<>());
-            idMap.put(type.getId(version), type);
-        }
-        return type;
+        TYPES_BUILDER.register(PATTERN_TYPE_MAP, PATTERN_TYPE_ID_MAP, pattern);
+        return pattern;
     }
 
     // with key
@@ -80,6 +76,48 @@ public class BannerPatterns {
         Map<Integer, BannerPattern> idMap = PATTERN_TYPE_ID_MAP.get((byte) index);
         return idMap.get(id);
     }
+
+    public static final BannerPattern SQUARE_BOTTOM_LEFT = define("square_bottom_left");
+    public static final BannerPattern STRIPE_BOTTOM = define("stripe_bottom");
+    public static final BannerPattern CREEPER = define("creeper");
+    public static final BannerPattern HALF_HORIZONTAL = define("half_horizontal");
+    public static final BannerPattern STRIPE_MIDDLE = define("stripe_middle");
+    public static final BannerPattern BASE = define("base");
+    public static final BannerPattern DIAGONAL_UP_RIGHT = define("diagonal_up_right");
+    public static final BannerPattern HALF_HORIZONTAL_BOTTOM = define("half_horizontal_bottom");
+    public static final BannerPattern SMALL_STRIPES = define("small_stripes");
+    public static final BannerPattern GRADIENT_UP = define("gradient_up");
+    public static final BannerPattern CIRCLE = define("circle");
+    public static final BannerPattern STRIPE_DOWNLEFT = define("stripe_downleft");
+    public static final BannerPattern RHOMBUS = define("rhombus");
+    public static final BannerPattern TRIANGLES_BOTTOM = define("triangles_bottom");
+    public static final BannerPattern STRIPE_CENTER = define("stripe_center");
+    public static final BannerPattern SQUARE_BOTTOM_RIGHT = define("square_bottom_right");
+    public static final BannerPattern DIAGONAL_RIGHT = define("diagonal_right");
+    public static final BannerPattern MOJANG = define("mojang");
+    public static final BannerPattern STRIPE_LEFT = define("stripe_left");
+    public static final BannerPattern SQUARE_TOP_LEFT = define("square_top_left");
+    public static final BannerPattern TRIANGLE_BOTTOM = define("triangle_bottom");
+    public static final BannerPattern SKULL = define("skull");
+    public static final BannerPattern SQUARE_TOP_RIGHT = define("square_top_right");
+    public static final BannerPattern GLOBE = define("globe");
+    public static final BannerPattern STRIPE_TOP = define("stripe_top");
+    public static final BannerPattern CROSS = define("cross");
+    public static final BannerPattern BRICKS = define("bricks");
+    public static final BannerPattern HALF_VERTICAL = define("half_vertical");
+    public static final BannerPattern STRIPE_DOWNRIGHT = define("stripe_downright");
+    public static final BannerPattern TRIANGLES_TOP = define("triangles_top");
+    public static final BannerPattern STRIPE_RIGHT = define("stripe_right");
+    public static final BannerPattern DIAGONAL_UP_LEFT = define("diagonal_up_left");
+    public static final BannerPattern HALF_VERTICAL_RIGHT = define("half_vertical_right");
+    public static final BannerPattern TRIANGLE_TOP = define("triangle_top");
+    public static final BannerPattern FLOWER = define("flower");
+    public static final BannerPattern STRAIGHT_CROSS = define("straight_cross");
+    public static final BannerPattern GRADIENT = define("gradient");
+    public static final BannerPattern CURLY_BORDER = define("curly_border");
+    public static final BannerPattern BORDER = define("border");
+    public static final BannerPattern PIGLIN = define("piglin");
+    public static final BannerPattern DIAGONAL_LEFT = define("diagonal_left");
 
     static {
         TYPES_BUILDER.unloadFileMappings();
