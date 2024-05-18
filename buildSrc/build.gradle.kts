daed712bf77431273522acdae54d0699954e33b5
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-05-19 04:21:38 +0530

    Version catalog for buildSrc
    Included version catalog for buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.johnrengelman:shadow:8.1.1")
-    compileOnly("org.jetbrains:annotations:23.0.0")
-    implementation("com.viaversion:nbt:4.0.0")
-    implementation("it.unimi.dsi:fastutil:8.5.6")
-    implementation("com.google.code.gson:gson:2.8.9")
-    implementation("io.github.java-diff-utils:java-diff-utils:4.12")
+    implementation(libs.shadow)
+    compileOnly(libs.jetbrains.annotations)
+    implementation(libs.via.nbt)
+    implementation(libs.fast.util)
+    implementation(libs.gson)
+    implementation(libs.java.diff.utils)
 }
