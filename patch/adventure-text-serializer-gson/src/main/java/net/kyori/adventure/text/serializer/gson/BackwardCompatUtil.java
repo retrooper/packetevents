9c4d7cb93395fbdf2ead5b97fad61a43e935a3ab
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-06-03 22:54:25 +0530

    Adventure version resolution fix

--- patch/adventure-text-serializer-gson/src/main/java/net/kyori/adventure/text/serializer/gson/BackwardCompatUtil.java
+++ patch/adventure-text-serializer-gson/src/main/java/net/kyori/adventure/text/serializer/gson/BackwardCompatUtil.java
@@ -1,9 +1,25 @@
+/*
+ * This file is part of packetevents - https://github.com/retrooper/packetevents
+ * Copyright (C) 2024 retrooper and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.kyori.adventure.text.serializer.gson;
 
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.nbt.api.BinaryTagHolder;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TranslationArgument;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.util.Codec;
 import org.jetbrains.annotations.NotNull;
@@ -18,7 +34,7 @@ public final class BackwardCompatUtil {
 
     static {
         try {
-            TranslationArgument.class.getName();
+            Component.translatable().arguments(Component.empty()); // TranslatableComponent#arguments method was added in 4.15.0
             IS_4_15_0_OR_NEWER = true;
         } catch (Throwable ignored) {
         }
