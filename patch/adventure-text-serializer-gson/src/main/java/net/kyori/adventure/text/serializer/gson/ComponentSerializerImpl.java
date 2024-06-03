baec4bdd00144e7dd2a9bb64692616ce3714acab
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-06-03 22:54:11 +0530

    MIT Licence

--- patch/adventure-text-serializer-gson/src/main/java/net/kyori/adventure/text/serializer/gson/ComponentSerializerImpl.java
+++ patch/adventure-text-serializer-gson/src/main/java/net/kyori/adventure/text/serializer/gson/ComponentSerializerImpl.java
@@ -1,3 +1,26 @@
+/*
+ * This file is part of adventure, licensed under the MIT License.
+ *
+ * Copyright (c) 2017-2024 KyoriPowered
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package net.kyori.adventure.text.serializer.gson;
 
 import com.google.gson.Gson;
@@ -257,9 +280,9 @@ final class ComponentSerializerImpl extends TypeAdapter<Component> {
             }
             // packetevents patch start
             boolean argsPresent;
-            try {
+            if (BackwardCompatUtil.IS_4_15_0_OR_NEWER){
                 argsPresent = !translatable.arguments().isEmpty();
-            } catch (final NoSuchMethodError e) {
+            } else  {
                 argsPresent = !translatable.args().isEmpty();
             }
             if (argsPresent) {
@@ -267,11 +290,7 @@ final class ComponentSerializerImpl extends TypeAdapter<Component> {
                 if (BackwardCompatUtil.IS_4_15_0_OR_NEWER) {
                     this.gson.toJson(translatable.arguments(), TRANSLATABLE_ARGUMENT_LIST_TYPE, out);
                 } else {
-                    try {
-                        this.gson.toJson(translatable.arguments(), COMPONENT_LIST_TYPE, out);
-                    } catch (final NoSuchMethodError e) {
-                        this.gson.toJson(translatable.args(), COMPONENT_LIST_TYPE, out);
-                    }
+                    this.gson.toJson(translatable.args(), COMPONENT_LIST_TYPE, out);
                 }
             }
             // packetevents patch end
