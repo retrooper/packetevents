5e414bae5853ac9bbd4f60bb5a83882777435661
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-06-07 02:48:55 +0530

    Revert "Patch for group name"
    This reverts commit 990dfc279f5bbecb2dd85ad2878ae5912ded1ab9.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-group = "com.github.retrooper.packetevents"
+group = "com.github.retrooper"
 description = rootProject.name
 version = "2.3.1-SNAPSHOT" //TODO UPDATE - ADD "-SNAPSHOT" if we are dealing with snapshot versions
 
