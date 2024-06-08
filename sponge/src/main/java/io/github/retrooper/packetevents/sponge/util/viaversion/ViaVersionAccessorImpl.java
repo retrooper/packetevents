77bd7fbf76b4975c3a1cba6c7576a9a080c16bb3
SamB440 <sam@islandearth.net>
2024-06-08 14:13:38 +0100

    Fix ViaVersion compatibility (as via fixed injector)

--- sponge/src/main/java/io/github/retrooper/packetevents/sponge/util/viaversion/ViaVersionAccessorImpl.java
+++ sponge/src/main/java/io/github/retrooper/packetevents/sponge/util/viaversion/ViaVersionAccessorImpl.java
@@ -1,34 +1,50 @@
-/*
- * This file is part of packetevents - https://github.com/retrooper/packetevents
- * Copyright (C) 2024 retrooper and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package io.github.retrooper.packetevents.sponge.util.viaversion;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.protocol.player.User;
+import com.github.retrooper.packetevents.util.reflection.Reflection;
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.connection.UserConnection;
-import com.viaversion.viaversion.sponge.handlers.SpongeDecodeHandler;
-import com.viaversion.viaversion.sponge.handlers.SpongeEncodeHandler;
+import io.netty.channel.Channel;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 
+import java.lang.reflect.Field;
+
 public class ViaVersionAccessorImpl implements ViaVersionAccessor {
+
+    private static final Class<?> ENCODE_HANDLER;
+    private static final Class<?> DECODE_HANDLER;
+
+    static {
+        try {
+            ENCODE_HANDLER = Class.forName("com.viaversion.sponge.handlers.SpongeEncodeHandler");
+            DECODE_HANDLER = Class.forName("com.viaversion.sponge.handlers.SpongeDecodeHandler");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Field CONNECTION_FIELD;
 
     @Override
     public int getProtocolVersion(ServerPlayer player) {
         return Via.getAPI().getPlayerVersion(player);
+    }
+
+    @Override
+    public int getProtocolVersion(User user) {
+        try {
+            Object viaEncoder = ((Channel) user.getChannel()).pipeline().get("via-encoder");
+            if (CONNECTION_FIELD == null) {
+                CONNECTION_FIELD = Reflection.getField(viaEncoder.getClass(), "info");
+            }
+            UserConnection connection = (UserConnection) CONNECTION_FIELD.get(viaEncoder);
+            return connection.getProtocolInfo().getProtocolVersion();
+        }
+        catch (IllegalAccessException e) {
+            PacketEvents.getAPI().getLogManager().warn("Unable to grab ViaVersion client version for player!");
+            return -1;
+        }
     }
 
     @Override
@@ -38,11 +54,11 @@ public class ViaVersionAccessorImpl implements ViaVersionAccessor {
 
     @Override
     public Class<?> getSpongeDecodeHandlerClass() {
-        return SpongeDecodeHandler.class;
+        return DECODE_HANDLER;
     }
 
     @Override
     public Class<?> getSpongeEncodeHandlerClass() {
-        return SpongeEncodeHandler.class;
+        return ENCODE_HANDLER;
     }
 }
