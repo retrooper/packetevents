8034de3ec3e241e6baa08bab134d4bd88cf55be1
Bram <bram@bramdekker.com>
2024-06-08 10:36:28 +0200

    Improved: JavaDocs

--- api/src/main/java/com/github/retrooper/packetevents/util/PEVersion.java
+++ api/src/main/java/com/github/retrooper/packetevents/util/PEVersion.java
@@ -25,9 +25,8 @@ import java.util.Objects;
 import java.util.Optional;
 
 /**
- * PacketEvents version.
- * This class represents a PacketEvents version using Semantic Versioning.
- * It supports comparison and cloning operations.
+ * Represents a PacketEvents version using Semantic Versioning.
+ * Supports version comparison, cloning, and provides a string representation.
  */
 public class PEVersion implements Comparable<PEVersion> {
 
@@ -37,12 +36,12 @@ public class PEVersion implements Comparable<PEVersion> {
     private final boolean snapshot;
 
     /**
-     * Specify your version using Semantic Versioning.
+     * Constructs a {@link PEVersion} instance.
      *
      * @param major    the major version number.
-     * @param minor    the minor version number
+     * @param minor    the minor version number.
      * @param patch    the patch version number.
-     * @param snapshot boolean flag indicating whether the version is a snapshot.
+     * @param snapshot whether the version is a snapshot.
      */
     public PEVersion(final int major, final int minor, final int patch, final boolean snapshot) {
         this.major = major;
@@ -52,7 +51,7 @@ public class PEVersion implements Comparable<PEVersion> {
     }
 
     /**
-     * Overloaded constructor for PEVersion where snapshot is defaulted to false.
+     * Constructs a {@link PEVersion} instance with snapshot defaulted to false.
      *
      * @param major the major version number.
      * @param minor the minor version number.
@@ -63,9 +62,10 @@ public class PEVersion implements Comparable<PEVersion> {
     }
 
     /**
-     * Specify your version using a string, for example, "1.8.9-SNAPSHOT".
+     * Constructs a {@link PEVersion} instance from a version string.
      *
-     * @param version String version.
+     * @param version the version string (e.g., "1.8.9-SNAPSHOT").
+     * @throws IllegalArgumentException if the version string format is incorrect.
      */
     public PEVersion(@NotNull final String version) {
         this.snapshot = version.endsWith("-SNAPSHOT");
@@ -82,9 +82,9 @@ public class PEVersion implements Comparable<PEVersion> {
     }
 
     /**
-     * Creates a PEVersion instance from the package version.
+     * Creates a {@link PEVersion} instance from the package implementation version.
      *
-     * @return PEVersion instance with a version derived from the package implementation version.
+     * @return a {@link PEVersion} instance.
      */
     public static PEVersion createFromPackageVersion() {
         String version = Optional.ofNullable(PacketEvents.class.getPackage().getImplementationVersion()).orElse("0.0.0");
@@ -92,47 +92,47 @@ public class PEVersion implements Comparable<PEVersion> {
     }
 
     /**
-     * Retreives the major version number
+     * Gets the major version number.
      *
-     * @return major version number
+     * @return the major version number.
      */
     public int major() {
         return major;
     }
 
     /**
-     * Retrieves the minor version number
+     * Gets the minor version number.
      *
-     * @return minor version number
+     * @return the minor version number.
      */
     public int minor() {
         return minor;
     }
 
     /**
-     * Retrieves the patch version number
+     * Gets the patch version number.
      *
-     * @return patch version number
+     * @return the patch version number.
      */
     public int patch() {
         return patch;
     }
 
     /**
-     * Checks if the version is a snapshot
+     * Checks if the version is a snapshot.
      *
-     * @return true if snapshot, false otherwise
+     * @return true if snapshot, false otherwise.
      */
     public boolean snapshot() {
         return snapshot;
     }
 
     /**
-     * Compares this PEVersion instance with another PEVersion.
-     * Considers major, minor, patch versions, and snapshot state.
+     * Compares this {@link PEVersion} with another {@link PEVersion}.
      *
-     * @param other The PEVersion object to be compared.
-     * @return A negative integer, zero, or a positive integer as this version is less than, equal to, or greater than the specified version.
+     * @param other the other {@link PEVersion}.
+     * @return a negative integer, zero, or a positive integer as this version is less than,
+     * equal to, or greater than the specified version.
      */
     @Override
     public int compareTo(@NotNull final PEVersion other) {
@@ -145,16 +145,14 @@ public class PEVersion implements Comparable<PEVersion> {
         int patchCompare = Integer.compare(this.patch, other.patch);
         if (patchCompare != 0) return patchCompare;
 
-        // Snapshot versions are considered older than non-snapshot versions
         return Boolean.compare(other.snapshot, this.snapshot);
     }
 
     /**
-     * Determines whether the provided object is equal to the current PEVersion.
-     * Checks for equality by comparing major, minor, patch versions, and the snapshot state.
+     * Checks if the provided object is equal to this {@link PEVersion}.
      *
-     * @param obj Object expected to be a PEVersion instance that is to be compared with the current PEVersion.
-     * @return Boolean, true if the provided object is logically equal to the current PEVersion, false otherwise.
+     * @param obj the object to compare.
+     * @return true if the provided object is equal to this {@link PEVersion}, false otherwise.
      */
     @Override
     public boolean equals(@NotNull final Object obj) {
@@ -171,8 +169,8 @@ public class PEVersion implements Comparable<PEVersion> {
     /**
      * Checks if this version is newer than the provided version.
      *
-     * @param otherVersion Other PEVersion.
-     * @return boolean, true if this version is newer, false otherwise.
+     * @param otherVersion the other {@link PEVersion}.
+     * @return true if this version is newer, false otherwise.
      */
     public boolean isNewerThan(@NotNull final PEVersion otherVersion) {
         return this.compareTo(otherVersion) > 0;
@@ -181,17 +179,17 @@ public class PEVersion implements Comparable<PEVersion> {
     /**
      * Checks if this version is older than the provided version.
      *
-     * @param otherVersion Other PEVersion.
-     * @return boolean, true if this version is older, false otherwise.
+     * @param otherVersion the other {@link PEVersion}.
+     * @return true if this version is older, false otherwise.
      */
     public boolean isOlderThan(@NotNull final PEVersion otherVersion) {
         return this.compareTo(otherVersion) < 0;
     }
 
     /**
-     * Returns a hash code value for the object based on its state.
+     * Returns a hash code value for this {@link PEVersion}.
      *
-     * @return A hash code value for this object.
+     * @return a hash code value.
      */
     @Override
     public int hashCode() {
@@ -199,9 +197,9 @@ public class PEVersion implements Comparable<PEVersion> {
     }
 
     /**
-     * Creates and returns a copy of this PEVersion instance.
+     * Creates and returns a copy of this {@link PEVersion}.
      *
-     * @return A clone of this instance.
+     * @return a clone of this instance.
      */
     @Override
     public PEVersion clone() {
@@ -209,14 +207,12 @@ public class PEVersion implements Comparable<PEVersion> {
     }
 
     /**
-     * Converts the PEVersion instance to a string representation.
-     * Constructed by concatenating major, minor, patch versions, and snapshot state.
+     * Converts the {@link PEVersion} to a string representation.
      *
-     * @return A string representation of the version in the pattern "Major.Minor.Patch-SnapshotStatus".
+     * @return a string representation of the version.
      */
     @Override
     public String toString() {
         return major + "." + minor + "." + patch + (snapshot ? "-SNAPSHOT" : "");
     }
 }
-
