4eaf000deaea89c0579cbb0963c2d91a184b74ef
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-06-22 03:16:19 +0530

    fix: Fixes NoSuchMethodError for GsonComponentSerializer on version 1.16.5
    Fixed NoSuchMethodError in GsonComponentSerializer due to older version of abstract classes in version 1.16.5
    
    Ticket: #832

--- buildSrc/src/main/kotlin/packetevents.shadow-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/packetevents.shadow-conventions.gradle.kts
@@ -11,9 +11,7 @@ tasks {
         archiveFileName = "packetevents-${project.name}-${project.version}.jar"
         archiveClassifier = null
 
-        relocate("net.kyori.adventure.text.serializer.gson", "io.github.retrooper.packetevents.adventure.serializer.gson")
-        relocate("net.kyori.adventure.text.serializer.json", "io.github.retrooper.packetevents.adventure.serializer.json")
-        relocate("net.kyori.adventure.text.serializer.legacy", "io.github.retrooper.packetevents.adventure.serializer.legacy")
+        relocate("net.kyori.adventure.text.serializer", "io.github.retrooper.packetevents.adventure.serializer")
         relocate("net.kyori.option", "io.github.retrooper.packetevents.adventure.option")
         dependencies {
             exclude(dependency("com.google.code.gson:gson:.*"))
