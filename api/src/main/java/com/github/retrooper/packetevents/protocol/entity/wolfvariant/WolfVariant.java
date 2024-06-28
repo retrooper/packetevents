16c71fad14db63f28611a20b788fdd0447f29330
retrooper <retrooperdev@gmail.com>
2024-06-29 00:47:38 +0200

    Start working on WolfVariant in EntityMetadata packet.

---
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/entity/wolfvariant/WolfVariant.java
@@ -0,0 +1,14 @@
+package com.github.retrooper.packetevents.protocol.entity.wolfvariant;
+
+import com.github.retrooper.packetevents.resources.ResourceLocation;
+
+import java.util.Set;
+
+//TODO Finish Wolf-Variant class
+public class WolfVariant {
+    private ResourceLocation wildId, tameId, angryId;
+
+    public WolfVariant(ResourceLocation wildId, ResourceLocation tameId, ResourceLocation angryId) {
+
+    }
+}
