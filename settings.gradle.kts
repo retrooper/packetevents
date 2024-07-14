2c806b2d4eb8b4b00a1f7aa36a3a8dc49e088edc
Bram <bram@bramdekker.com>
2024-07-14 11:17:40 +0200

    Removed: foojay-resolver in favor of the foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,17 +23,6 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-    id("org.gradle.toolchains.foojay-resolver") version "0.8.0"
-}
-
-toolchainManagement {
-    jvm {
-        javaRepositories {
-            repository("foojay") {
-                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
-            }
-        }
-    }
 }
 
 rootProject.name = "packetevents"
