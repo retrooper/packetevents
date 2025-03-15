0c820df1c0fedf0096138c920101496e37fe43b4
oop778 <oskardhavel@gmail.com>
2025-03-15 09:48:34 +0200

    Tests for Weak Key eviction & call order

---
+++ api/src/test/java/com/github/retrooper/packetevents/test/event/InheritableEventManagerTest.java
@@ -0,0 +1,62 @@
+package com.github.retrooper.packetevents.test.event;
+
+import com.github.retrooper.packetevents.event.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InheritableEventManagerTest {
+    static PacketListenerCommon mockListener() {
+        return mockListener(PacketListenerPriority.NORMAL, () -> {});
+    }
+
+    static PacketListenerCommon mockListener(Runnable onAccept) {
+        return mockListener(PacketListenerPriority.NORMAL, onAccept);
+    }
+
+    static PacketListenerCommon mockListener(PacketListenerPriority priority, Runnable onAccept) {
+        return new PacketListenerCommon(priority) {
+            @Override
+            public void onUserConnect(UserConnectEvent event) {
+                super.onUserConnect(event);
+                onAccept.run();
+            }
+        };
+    }
+
+    @Test
+    void shouldDestroyChildrenAfterGC() throws Throwable {
+        final RootEventManager rootEventManager = new RootEventManager();
+        Object children = new Object();
+
+        rootEventManager.getChildren(children);
+        rootEventManager.getChildren(children);
+        assertEquals(1, rootEventManager.childrenCount());
+
+        children = null;
+        System.gc();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        assertEquals(0, rootEventManager.childrenCount());
+    }
+
+    @Test
+    void shouldCallBothListeners() {
+        final RootEventManager rootEventManager = new RootEventManager();
+        final EventManager children = rootEventManager.getChildren(new Object());
+        final List<String> calls = new ArrayList<>();
+
+        children.registerListener(mockListener(() -> calls.add("children")));
+        rootEventManager.registerListener(mockListener(() -> calls.add("root")));
+
+        children.callEvent(new UserConnectEvent(null));
+
+        assertEquals(Arrays.asList("children", "root"), calls);
+    }
+}
