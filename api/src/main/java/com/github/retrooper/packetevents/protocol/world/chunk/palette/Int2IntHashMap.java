cceeb0c999ce4ed353a2480af9b8013ec4b1078f
booky <booky@booky.dev>
2026-01-16 19:05:07 +0100

    Improve Int2IntHashMap by using long-array table
    Stores key and value in one long to reduce array reads/writes
    Also improves resizing logic by removing unnecessary replacement logic check
    
    (Additionally also includes some minor code format changes)

--- api/src/main/java/com/github/retrooper/packetevents/protocol/world/chunk/palette/Int2IntHashMap.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/world/chunk/palette/Int2IntHashMap.java
@@ -1,6 +1,7 @@
 package com.github.retrooper.packetevents.protocol.world.chunk.palette;
 
 import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.NullMarked;
 
 import java.util.Arrays;
 
@@ -11,20 +12,21 @@ import java.util.Arrays;
  * <p>
  * Greatly inspired by <a href="https://github.com/aeron-io/agrona/blob/5512dda83e1b1a8dc38a6286e440237b1a7e480c/agrona/src/main/java/org/agrona/collections/Int2IntHashMap.java">Int2IntHashMap.java</a>, licensed under the terms of the Apache License 2.0.
  */
+@NullMarked
 @ApiStatus.Internal
-class Int2IntHashMap {
+final class Int2IntHashMap {
 
     private static final float LOAD_FACTOR = 0.75f;
     public static final int EMPTY_VALUE = -1;
 
-    private int[] data;
+    private long[] data;
     private int size;
     private int threshold;
 
     public Int2IntHashMap(int expectedSize) {
         int capacity = tableSizeFor(expectedSize);
         this.threshold = (int) (capacity * LOAD_FACTOR);
-        this.data = new int[capacity * 2];
+        this.data = new long[capacity];
         Arrays.fill(this.data, EMPTY_VALUE);
     }
 
@@ -34,69 +36,82 @@ class Int2IntHashMap {
     }
 
     public int get(int key) {
-        int capacity = data.length >> 1;
-        int mask = capacity - 1;
-        int index = hash(key) & mask;
-        while (data[index * 2] != EMPTY_VALUE) {
-            if (data[index * 2] == key) {
-                return data[index * 2 + 1];
+        int mask = this.data.length - 1;
+        int index = this.hash(key) & mask;
+        long entry;
+        while ((entry = this.data[index]) != EMPTY_VALUE) {
+            if ((int) (entry & 0xFFFFFFFFL) == key) {
+                return (int) (entry >>> 32);
             }
+            // collision, advance
             index = (index + 1) & mask;
         }
         return EMPTY_VALUE;
     }
 
     public void put(int key, int value) {
-        if (size >= threshold) {
-            resize((data.length >> 1) * 2);
+        if (this.size >= this.threshold) {
+            this.resize(this.data.length << 1);
         }
-        int capacity = data.length >> 1;
-        int mask = capacity - 1;
-        int index = hash(key) & mask;
-        while (data[index * 2] != EMPTY_VALUE) {
-            if (data[index * 2] == key) {
-                data[index * 2 + 1] = value;
+        long[] data = this.data;
+        int mask = data.length - 1;
+        int index = this.hash(key) & mask;
+        long entry;
+        while ((entry = data[index]) != EMPTY_VALUE) {
+            if ((int) (entry & 0xFFFFFFFFL) == key) {
+                // replace existing entry
+                data[index] = (entry & 0xFFFFFFFFL) | ((value & 0xFFFFFFFFL) << 32);
                 return;
             }
+            // collision, advance
             index = (index + 1) & mask;
         }
-        data[index * 2] = key;
-        data[index * 2 + 1] = value;
-        size++;
+        // insert new entry at empty index
+        data[index] = (key & 0xFFFFFFFFL) | ((value & 0xFFFFFFFFL) << 32);
+        this.size++;
     }
 
     public void putIfAbsent(int key, int value) {
-        if (size >= threshold) {
-            resize((data.length >> 1) * 2);
+        if (this.size >= this.threshold) {
+            this.resize(this.data.length << 1);
         }
-        int capacity = data.length >> 1;
-        int mask = capacity - 1;
-        int index = hash(key) & mask;
-        while (data[index * 2] != EMPTY_VALUE) {
-            if (data[index * 2] == key) {
-                return;
+        long[] data = this.data;
+        int mask = data.length - 1;
+        int index = this.hash(key) & mask;
+        long entry;
+        while ((entry = data[index]) != EMPTY_VALUE) {
+            if ((int) (entry & 0xFFFFFFFFL) == key) {
+                return; // already present, don't insert
             }
+            // collision, advance
             index = (index + 1) & mask;
         }
-        data[index * 2] = key;
-        data[index * 2 + 1] = value;
-        size++;
+        // insert new entry at empty index
+        data[index] = (key & 0xFFFFFFFFL) | ((value & 0xFFFFFFFFL) << 32);
+        this.size++;
     }
 
     private void resize(int newCapacity) {
-        int[] oldData = data;
-        int oldCapacity = oldData.length >> 1;
+        long[] oldData = this.data;
 
-        threshold = (int) (newCapacity * LOAD_FACTOR);
-        data = new int[newCapacity * 2];
+        // construct new table
+        this.threshold = (int) (newCapacity * LOAD_FACTOR);
+        long[] data = new long[newCapacity];
         Arrays.fill(data, EMPTY_VALUE);
-        size = 0;
+        this.data = data;
 
-        for (int i = 0; i < oldCapacity; i++) {
-            int key = oldData[i * 2];
-            if (key != EMPTY_VALUE) {
-                put(key, oldData[i * 2 + 1]);
+        // put old table back into new table
+        int mask = newCapacity - 1;
+        for (int i = 0, len = oldData.length; i < len; i++) {
+            long entry = oldData[i];
+            int key = (int) (entry & 0xFFFFFFFFL);
+            int index = this.hash(key) & mask;
+            // we can skip checking for matches against this same key as
+            // this is a fresh table being built and there should be no replacements, only collisions
+            while (data[index] != EMPTY_VALUE) {
+                index = (index + 1) & mask;
             }
+            data[index] = entry;
         }
     }
 
