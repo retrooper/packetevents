7859850cf1ffce3308c51b7b209a2799b2b5153b
booky <booky@booky.dev>
2026-04-05 21:42:15 +0200

    Bump dependencies for paper 26.1.1 alpha builds

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -37,8 +37,8 @@ tasks {
     // 1.18 - 1.20.4    = Java 17
     // 1.20.5 - 1.21.11 = Java 21
     // 26.1+            = Java 25
-    val version = "1.21.11"
-    val javaVersion = JavaLanguageVersion.of(21)
+    val version = "26.1.1"
+    val javaVersion = JavaLanguageVersion.of(25)
 
     val jvmArgsExternal = listOf(
         "-Dcom.mojang.eula.agree=true"
