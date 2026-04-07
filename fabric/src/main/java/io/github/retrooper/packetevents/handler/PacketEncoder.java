275da3343a42e21d741ac8a97b9156f89c7942d8
booky <booky@booky.dev>
2026-04-07 04:53:52 +0200

    Fix missing tasks-after-send handling for fabric
    Why does this list of runnables only exist for clientbound packets? Doesn't make sense to me

--- fabric/src/main/java/io/github/retrooper/packetevents/handler/PacketEncoder.java
+++ fabric/src/main/java/io/github/retrooper/packetevents/handler/PacketEncoder.java
@@ -18,6 +18,9 @@
 
 package io.github.retrooper.packetevents.handler;
 
+import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.event.ProtocolPacketEvent;
+import com.github.retrooper.packetevents.exception.PacketProcessException;
 import com.github.retrooper.packetevents.protocol.PacketSide;
 import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.util.PacketEventsImplHelper;
@@ -51,10 +54,21 @@ public class PacketEncoder extends ChannelOutboundHandlerAdapter {
             return;
         }
 
-        PacketEventsImplHelper.handlePacket(ctx.channel(),
+        ProtocolPacketEvent event = PacketEventsImplHelper.handlePacket(ctx.channel(),
                 this.user, this.player, in, false, this.side);
         if (in.isReadable()) {
             ctx.write(in, promise);
+        }
+
+        // TODO why do these tasks only exist for the "send" (clientbound) event?
+        if (event instanceof PacketSendEvent sendEvent && sendEvent.hasTasksAfterSend()) {
+            for (Runnable task : sendEvent.getTasksAfterSend()) {
+                try {
+                    task.run();
+                } catch (Throwable throwable) {
+                    throw new PacketProcessException("Error while handling post-send-task " + task + " for " + event, throwable);
+                }
+            }
         }
     }
 }
