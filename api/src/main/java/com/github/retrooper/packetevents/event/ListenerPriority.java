e5198d447af9e24242651be58e9ccc4a45bf1cc0
booky <booky@booky.dev>
2026-04-28 15:30:01 +0200

    Cleanup/reformat ListenerPriority

--- api/src/main/java/com/github/retrooper/packetevents/event/ListenerPriority.java
+++ api/src/main/java/com/github/retrooper/packetevents/event/ListenerPriority.java
@@ -1,19 +1,39 @@
+/*
+ * This file is part of packetevents - https://github.com/retrooper/packetevents
+ * Copyright (C) 2026 retrooper and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.retrooper.packetevents.event;
 
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NullMarked;
 
 /**
- *  Class for defining listener priorities.
- *  The priority of packet listeners affect the order they will be invoked in.
- *  The lowest priority listeners are invoked first, the highest are invoked last.
- *  The highest priority listener has the final decider on an event being cancelled.
- *  <p>
- *  Users can define custom priorities by using the {@link #custom(int)} method.
- *  <p>
- *  Predefined priorities are: {@link #LOWEST}, {@link #LOW}, {@link #NORMAL}, {@link #HIGH}, {@link #HIGHEST} and {@link #MONITOR}.
- *  @author ieatglu3
+ * Class for defining listener priorities.
+ * The priority of packet listeners affect the order they will be invoked in.
+ * The lowest priority listeners are invoked first, the highest are invoked last.
+ * The highest priority listener has the final decider on an event being cancelled.
+ * <p>
+ * Users can define custom priorities by using the {@link #custom(int)} method.
+ * <p>
+ * Predefined priorities are: {@link #LOWEST}, {@link #LOW}, {@link #NORMAL}, {@link #HIGH}, {@link #HIGHEST} and {@link #MONITOR}.
+ *
+ * @author ieatglu3
  */
+@NullMarked
 public final class ListenerPriority implements Comparable<ListenerPriority> {
 
     private final int ordinal;
@@ -82,6 +102,7 @@ public final class ListenerPriority implements Comparable<ListenerPriority> {
      * Create a custom listener priority with the specified ordinal. The higher the ordinal, the higher the priority.
      * <p>
      * The predefined priorities have ordinals from 0 to 5. Custom priorities can be negative.
+     *
      * @param ordinal the ordinal of this priority
      * @return a new {@link ListenerPriority} with the specified ordinal
      */
@@ -96,28 +117,41 @@ public final class ListenerPriority implements Comparable<ListenerPriority> {
         return VALUES.clone();
     }
 
-    @ApiStatus.Internal
+    @Deprecated
     public static ListenerPriority fromLegacy(PacketListenerPriority priority) {
         switch (priority) {
-            case LOWEST: return LOWEST;
-            case LOW: return LOW;
-            case NORMAL: return NORMAL;
-            case HIGH: return HIGH;
-            case HIGHEST: return HIGHEST;
-            case MONITOR: return MONITOR;
-            default: throw new IllegalArgumentException("unknown legacy priority: " + priority);
+            case LOWEST:
+                return LOWEST;
+            case LOW:
+                return LOW;
+            case NORMAL:
+                return NORMAL;
+            case HIGH:
+                return HIGH;
+            case HIGHEST:
+                return HIGHEST;
+            case MONITOR:
+                return MONITOR;
+            default:
+                throw new IllegalArgumentException("unknown legacy priority: " + priority);
         }
     }
 
-    @ApiStatus.Internal
+    @Deprecated
     public PacketListenerPriority legacyType() {
         switch (this.ordinal) {
-            case 0: return PacketListenerPriority.LOWEST;
-            case 1: return PacketListenerPriority.LOW;
-            case 2: return PacketListenerPriority.NORMAL;
-            case 3: return PacketListenerPriority.HIGH;
-            case 4: return PacketListenerPriority.HIGHEST;
-            default: return PacketListenerPriority.MONITOR;
+            case 0:
+                return PacketListenerPriority.LOWEST;
+            case 1:
+                return PacketListenerPriority.LOW;
+            case 2:
+                return PacketListenerPriority.NORMAL;
+            case 3:
+                return PacketListenerPriority.HIGH;
+            case 4:
+                return PacketListenerPriority.HIGHEST;
+            default:
+                return PacketListenerPriority.MONITOR;
         }
     }
 }
