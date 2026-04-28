80f39e2feff2bcc8921576a7f62f784e983ad6fb
booky <booky@booky.dev>
2026-04-28 12:28:48 +0200

    Fix testserver version

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -44,8 +44,8 @@ tasks {
     // 1.18 - 1.20.4    = Java 17
     // 1.20.5 - 1.21.11 = Java 21
     // 26.1+            = Java 25
-    val version = "1.16.5"
-    val javaVersion = JavaLanguageVersion.of(8)
+    val version = "26.1.2"
+    val javaVersion = JavaLanguageVersion.of(25)
 
     val jvmArgsExternal = listOf(
         "-Dcom.mojang.eula.agree=true"
