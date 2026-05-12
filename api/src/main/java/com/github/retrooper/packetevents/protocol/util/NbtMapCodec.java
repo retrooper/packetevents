2fccee72cfceca10801046deb54d1a49112045fd
booky <booky@booky.dev>
2026-05-12 22:01:03 +0200

    Ignore unsynced environment attributes
    Matches behavior of vanilla client, fixes https://github.com/retrooper/packetevents/issues/1482

--- api/src/main/java/com/github/retrooper/packetevents/protocol/util/NbtMapCodec.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/util/NbtMapCodec.java
@@ -5,11 +5,13 @@ import com.github.retrooper.packetevents.protocol.nbt.NBTCompound;
 import com.github.retrooper.packetevents.protocol.nbt.NBTString;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 @NullMarked
 public interface NbtMapCodec<T> extends NbtMapEncoder<T>, NbtMapDecoder<T> {
@@ -19,12 +21,23 @@ public interface NbtMapCodec<T> extends NbtMapEncoder<T>, NbtMapDecoder<T> {
     }
 
     static <K, V> NbtMapCodec<Map<K, V>> codecOfMap(NbtCodec<K> keyCodec, Function<K, NbtCodec<? extends V>> valueCodec) {
+        return codecOfMap(keyCodec, null, valueCodec);
+    }
+
+    static <K, V> NbtMapCodec<Map<K, V>> codecOfMap(
+            NbtCodec<K> keyCodec,
+            @Nullable Predicate<K> keyPredicate,
+            Function<K, NbtCodec<? extends V>> valueCodec
+    ) {
         return new NbtMapCodec<Map<K, V>>() {
             @Override
             public Map<K, V> decode(NBTCompound compound, PacketWrapper<?> wrapper) throws NbtCodecException {
                 Map<K, V> map = new HashMap<>(compound.size());
                 for (Map.Entry<String, NBT> entry : compound.getTags().entrySet()) {
                     K key = keyCodec.decode(new NBTString(entry.getKey()), wrapper);
+                    if (keyPredicate != null && !keyPredicate.test(key)) {
+                        continue;
+                    }
                     V value = valueCodec.apply(key).decode(entry.getValue(), wrapper);
                     map.put(key, value);
                 }
@@ -34,6 +47,9 @@ public interface NbtMapCodec<T> extends NbtMapEncoder<T>, NbtMapDecoder<T> {
             @Override
             public void encode(NBTCompound compound, PacketWrapper<?> wrapper, Map<K, V> value) throws NbtCodecException {
                 for (Map.Entry<K, V> entry : value.entrySet()) {
+                    if (keyPredicate != null && !keyPredicate.test(entry.getKey())) {
+                        continue;
+                    }
                     String name = keyCodec.encode(wrapper, entry.getKey())
                             .castOrThrow(NBTString.class).getValue();
                     @SuppressWarnings("unchecked")
