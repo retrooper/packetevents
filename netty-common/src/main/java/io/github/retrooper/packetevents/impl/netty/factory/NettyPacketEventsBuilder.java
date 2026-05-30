retrooper/packetevents

commit d0a97997181a860a4e7b7dbc4c92ac62295b7bf7

Implement common load/termination logic

netty-common/src/main/java/io/github/retrooper/packetevents/impl/netty/factory/NettyPacketEventsBuilder.java
@@ -21,7 +21,6 @@ package io.github.retrooper.packetevents.impl.netty.factory;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.PacketEventsAPI;
 import com.github.retrooper.packetevents.injector.ChannelInjector;
-import com.github.retrooper.packetevents.manager.InternalPacketListener;
 import com.github.retrooper.packetevents.manager.player.PlayerManager;
 import com.github.retrooper.packetevents.manager.protocol.ProtocolManager;
 import com.github.retrooper.packetevents.manager.server.ServerManager;
@@ -95,13 +94,10 @@ public class NettyPacketEventsBuilder {
                     PacketEvents.CONNECTION_HANDLER_NAME = "pe-connection-handler-" + id;
                     PacketEvents.SERVER_CHANNEL_HANDLER_NAME = "pe-connection-initializer-" + id;
                     PacketEvents.TIMEOUT_HANDLER_NAME = "pe-timeout-handler-" + id;
-                    injector.inject();
+
+                    super.load();
 
                     loaded = true;
-
-                    //Register internal packet listener (should be the first listener)
-                    //This listener doesn't do any modifications to the packets, just reads data
-                    getEventManager().registerListener(new InternalPacketListener());
                 }
             }
 
@@ -135,10 +131,7 @@ public class NettyPacketEventsBuilder {
             @Override
             public void terminate() {
                 if (initialized) {
-                    //Uninject the injector if needed(depends on the injector implementation)
-                    injector.uninject();
-                    //Unregister all our listeners
-                    getEventManager().unregisterAllListeners();
+                    super.terminate();
                     initialized = false;
                     terminated = true;
                 }

velocity/src/main/java/io/github/retrooper/packetevents/velocity/factory/VelocityPacketEventsBuilder.java
@@ -22,7 +22,6 @@ import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.PacketEventsAPI;
 import com.github.retrooper.packetevents.event.UserLoginEvent;
 import com.github.retrooper.packetevents.injector.ChannelInjector;
-import com.github.retrooper.packetevents.manager.InternalPacketListener;
 import com.github.retrooper.packetevents.manager.player.PlayerManager;
 import com.github.retrooper.packetevents.manager.protocol.ProtocolManager;
 import com.github.retrooper.packetevents.manager.server.ServerManager;
@@ -31,10 +30,8 @@ import com.github.retrooper.packetevents.netty.NettyManager;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.protocol.player.User;
-import com.github.retrooper.packetevents.protocol.world.states.WrappedBlockState;
 import com.github.retrooper.packetevents.settings.PacketEventsSettings;
 import com.github.retrooper.packetevents.util.LogManager;
-import com.github.retrooper.packetevents.util.mappings.SynchronizedRegistriesHandler;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
 import com.velocitypowered.api.network.ProtocolVersion;
 import com.velocitypowered.api.plugin.PluginContainer;
@@ -179,15 +176,8 @@ public class VelocityPacketEventsBuilder {
                     PacketEvents.CONNECTION_HANDLER_NAME = "pe-connection-handler-" + id;
                     PacketEvents.SERVER_CHANNEL_HANDLER_NAME = "pe-connection-initializer-" + id;
                     PacketEvents.TIMEOUT_HANDLER_NAME = "pe-timeout-handler-" + id;
-                    WrappedBlockState.ensureLoad();
-                    SynchronizedRegistriesHandler.init();
-                    injector.inject();
-
+                    super.load();
                     loaded = true;
-
-                    // Register internal packet listener (should be the first listener)
-                    // This listener doesn't do any modifications to the packets, just reads data
-                    getEventManager().registerListener(new InternalPacketListener());
                 }
             }
 
@@ -236,13 +226,7 @@ public class VelocityPacketEventsBuilder {
             @Override
             public void terminate() {
                 if (initialized) {
-                    // try to uninject the injector
-                    try {
-                        this.injector.uninject();
-                    } catch (Exception exception) {
-                        this.logManager.warn("Failed to uninject from initializer");
-                        exception.printStackTrace();
-                    }
+                    super.terminate();
                     // Remove handlers for players
                     for (User user : this.protocolManager.getUsers()) {
                         Channel channel = (Channel) user.getChannel();
@@ -253,8 +237,6 @@ public class VelocityPacketEventsBuilder {
                             channel.pipeline().remove(PacketEvents.DECODER_NAME);
                         }
                     }
-                    // Unregister all our listeners
-                    getEventManager().unregisterAllListeners();
                     initialized = false;
                     terminated = true;
                 }

api/src/main/java/com/github/retrooper/packetevents/PacketEventsAPI.java
@@ -20,15 +20,18 @@ package com.github.retrooper.packetevents;
 
 import com.github.retrooper.packetevents.event.EventManager;
 import com.github.retrooper.packetevents.injector.ChannelInjector;
+import com.github.retrooper.packetevents.manager.InternalPacketListener;
 import com.github.retrooper.packetevents.manager.player.PlayerManager;
 import com.github.retrooper.packetevents.manager.protocol.ProtocolManager;
 import com.github.retrooper.packetevents.manager.server.ServerManager;
 import com.github.retrooper.packetevents.netty.NettyManager;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.world.states.WrappedBlockState;
 import com.github.retrooper.packetevents.settings.PacketEventsSettings;
 import com.github.retrooper.packetevents.util.LogManager;
 import com.github.retrooper.packetevents.util.PEVersion;
-//Note: The PEVersions class is generated. Refer to PEVersionTask.kt
 import com.github.retrooper.packetevents.util.PEVersions;
+import com.github.retrooper.packetevents.util.mappings.SynchronizedRegistriesHandler;
 import com.github.retrooper.packetevents.util.updatechecker.UpdateChecker;
 
 import java.util.logging.Logger;
@@ -65,7 +68,15 @@ public abstract class PacketEventsAPI<T> {
         return logManager;
     }
 
-    public abstract void load();
+    public void load() {
+        WrappedBlockState.ensureLoad();
+        SynchronizedRegistriesHandler.init();
+        PacketType.prepare();
+
+        // Register internal packet listener (should be the first listener)
+        // This listener doesn't do any modifications to the packets, just reads data
+        this.getEventManager().registerListener(new InternalPacketListener());
+    }
 
     public abstract boolean isLoaded();
 
@@ -73,7 +84,10 @@ public abstract class PacketEventsAPI<T> {
 
     public abstract boolean isInitialized();
 
-    public abstract void terminate();
+    public void terminate() {
+        this.getInjector().uninject();
+        this.getEventManager().unregisterAllListeners();
+    }
 
     public abstract boolean isTerminated();
 

bungeecord/src/main/java/io/github/retrooper/packetevents/bungee/factory/BungeePacketEventsBuilder.java
@@ -21,7 +21,6 @@ package io.github.retrooper.packetevents.bungee.factory;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.PacketEventsAPI;
 import com.github.retrooper.packetevents.injector.ChannelInjector;
-import com.github.retrooper.packetevents.manager.InternalPacketListener;
 import com.github.retrooper.packetevents.manager.player.PlayerManager;
 import com.github.retrooper.packetevents.manager.protocol.ProtocolManager;
 import com.github.retrooper.packetevents.manager.server.ServerManager;
@@ -34,10 +33,8 @@ import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.protocol.player.UserProfile;
-import com.github.retrooper.packetevents.protocol.world.states.WrappedBlockState;
 import com.github.retrooper.packetevents.settings.PacketEventsSettings;
 import com.github.retrooper.packetevents.util.LogManager;
-import com.github.retrooper.packetevents.util.mappings.SynchronizedRegistriesHandler;
 import io.github.retrooper.packetevents.impl.netty.NettyManagerImpl;
 import io.github.retrooper.packetevents.impl.netty.manager.player.PlayerManagerAbstract;
 import io.github.retrooper.packetevents.impl.netty.manager.protocol.ProtocolManagerAbstract;
@@ -202,15 +199,10 @@ public class BungeePacketEventsBuilder {
                     PacketEvents.SERVER_CHANNEL_HANDLER_NAME = "pe-connection-initializer-" + id;
                     PacketEvents.TIMEOUT_HANDLER_NAME = "pe-timeout-handler-" + id;
 
-                    WrappedBlockState.ensureLoad();
-                    SynchronizedRegistriesHandler.init();
+                    super.load();
 
                     injector.inject();
                     loaded = true;
-
-                    // Register internal packet listener (should be the first listener)
-                    // This listener doesn't do any modifications to the packets, just reads data
-                    getEventManager().registerListener(new InternalPacketListener());
                 }
             }
 
@@ -247,10 +239,7 @@ public class BungeePacketEventsBuilder {
             @Override
             public void terminate() {
                 if (initialized) {
-                    // Uninject the injector if needed(depends on the injector implementation)
-                    injector.uninject();
-                    // Unregister all our listeners
-                    getEventManager().unregisterAllListeners();
+                    super.terminate();
                     initialized = false;
                     terminated = true;
                 }

fabric-common/src/main/java/io/github/retrooper/packetevents/factory/fabric/FabricPacketEventsAPI.java
@@ -21,16 +21,13 @@ package io.github.retrooper.packetevents.factory.fabric;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.PacketEventsAPI;
 import com.github.retrooper.packetevents.injector.ChannelInjector;
-import com.github.retrooper.packetevents.manager.InternalPacketListener;
 import com.github.retrooper.packetevents.manager.player.PlayerManager;
 import com.github.retrooper.packetevents.manager.protocol.ProtocolManager;
 import com.github.retrooper.packetevents.manager.server.ServerManager;
 import com.github.retrooper.packetevents.netty.NettyManager;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.protocol.world.states.WrappedBlockState;
 import com.github.retrooper.packetevents.settings.PacketEventsSettings;
 import com.github.retrooper.packetevents.util.LogManager;
-import com.github.retrooper.packetevents.util.mappings.SynchronizedRegistriesHandler;
 import io.github.retrooper.packetevents.impl.netty.NettyManagerImpl;
 import io.github.retrooper.packetevents.impl.netty.manager.player.PlayerManagerAbstract;
 import net.fabricmc.api.EnvType;
@@ -93,12 +90,7 @@ public abstract class FabricPacketEventsAPI extends PacketEventsAPI<FabricLoader
         PacketEvents.CONNECTION_HANDLER_NAME = "pe-connection-handler-" + id;
         PacketEvents.SERVER_CHANNEL_HANDLER_NAME = "pe-connection-initializer-" + id;
 
-        WrappedBlockState.ensureLoad();
-        SynchronizedRegistriesHandler.init();
-
-        // register internal packet listener (should be the first listener)
-        // this listener doesn't do any modifications to the packets, just reads data
-        this.getEventManager().registerListener(new InternalPacketListener());
+        super.load();
         this.loaded = true;
     }
 
@@ -135,7 +127,7 @@ public abstract class FabricPacketEventsAPI extends PacketEventsAPI<FabricLoader
         if (!this.initialized) {
             return;
         }
-        this.getEventManager().unregisterAllListeners();
+        super.terminate();
         this.initialized = false;
         this.terminated = true;
     }

spigot/src/main/java/io/github/retrooper/packetevents/factory/spigot/SpigotPacketEventsBuilder.java
@@ -26,13 +26,10 @@ import com.github.retrooper.packetevents.manager.protocol.ProtocolManager;
 import com.github.retrooper.packetevents.manager.server.ServerManager;
 import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.netty.NettyManager;
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.User;
-import com.github.retrooper.packetevents.protocol.world.states.WrappedBlockState;
 import com.github.retrooper.packetevents.settings.PacketEventsSettings;
 import com.github.retrooper.packetevents.util.LogManager;
 import com.github.retrooper.packetevents.util.PEVersion;
-import com.github.retrooper.packetevents.util.mappings.SynchronizedRegistriesHandler;
 import com.github.retrooper.packetevents.util.updatechecker.UpdateChecker;
 import io.github.retrooper.packetevents.bukkit.InternalBukkitListener;
 import io.github.retrooper.packetevents.bukkit.InternalBukkitLoginListener;
@@ -41,7 +38,6 @@ import io.github.retrooper.packetevents.bukkit.InternalPaperJoinListener;
 import io.github.retrooper.packetevents.bukkit.InternalPaperListener;
 import io.github.retrooper.packetevents.injector.SpigotChannelInjector;
 import io.github.retrooper.packetevents.injector.connection.ServerConnectionInitializer;
-import io.github.retrooper.packetevents.manager.InternalBukkitPacketListener;
 import io.github.retrooper.packetevents.manager.player.PlayerManagerImpl;
 import io.github.retrooper.packetevents.manager.protocol.ProtocolManagerImpl;
 import io.github.retrooper.packetevents.manager.server.ServerManagerImpl;
@@ -115,13 +111,10 @@ public class SpigotPacketEventsBuilder {
                     try {
                         SpigotReflectionUtil.init();
                         CustomPipelineUtil.init();
-                        WrappedBlockState.ensureLoad();
-                        SynchronizedRegistriesHandler.init();
                     } catch (Exception ex) {
                         throw new IllegalStateException(ex);
                     }
-
-                    PacketType.prepare();
+                    super.load();
 
                     //Server hasn't bound to the port yet.
                     lateBind = !injector.isServerBound();
@@ -131,10 +124,6 @@ public class SpigotPacketEventsBuilder {
                     }
 
                     loaded = true;
-
-                    //Register internal packet listener (should be the first listener)
-                    //This listener doesn't do any modifications to the packets, just reads data
-                    getEventManager().registerListener(new InternalBukkitPacketListener());
                 }
             }
 
@@ -307,13 +296,10 @@ public class SpigotPacketEventsBuilder {
             @Override
             public void terminate() {
                 if (initialized) {
-                    //Uninject the injector if needed(depends on the injector implementation)
-                    injector.uninject();
+                    super.terminate();
                     for (User user : this.protocolManager.getUsers()) {
                         ServerConnectionInitializer.destroyHandlers(user.getChannel());
                     }
-                    //Unregister all listeners. Because if we attempt to reload, we will end up with duplicate listeners.
-                    getEventManager().unregisterAllListeners();
                     initialized = false;
                     terminated = true;
                 }

sponge/src/main/java/io/github/retrooper/packetevents/sponge/factory/SpongePacketEventsBuilder.java
@@ -25,23 +25,19 @@ import com.github.retrooper.packetevents.manager.player.PlayerManager;
 import com.github.retrooper.packetevents.manager.protocol.ProtocolManager;
 import com.github.retrooper.packetevents.manager.server.ServerManager;
 import com.github.retrooper.packetevents.netty.NettyManager;
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.User;
-import com.github.retrooper.packetevents.protocol.world.states.WrappedBlockState;
 import com.github.retrooper.packetevents.settings.PacketEventsSettings;
 import com.github.retrooper.packetevents.util.LogManager;
-import com.github.retrooper.packetevents.util.mappings.SynchronizedRegistriesHandler;
+import io.github.retrooper.packetevents.sponge.InternalSpongeListener;
 import io.github.retrooper.packetevents.sponge.injector.SpongeChannelInjector;
 import io.github.retrooper.packetevents.sponge.injector.connection.ServerConnectionInitializer;
-import io.github.retrooper.packetevents.sponge.manager.protocol.ProtocolManagerImpl;
-import io.github.retrooper.packetevents.sponge.util.viaversion.CustomPipelineUtil;
-import io.github.retrooper.packetevents.sponge.manager.InternalSpongePacketListener;
 import io.github.retrooper.packetevents.sponge.manager.player.PlayerManagerImpl;
+import io.github.retrooper.packetevents.sponge.manager.protocol.ProtocolManagerImpl;
 import io.github.retrooper.packetevents.sponge.manager.server.ServerManagerImpl;
 import io.github.retrooper.packetevents.sponge.netty.NettyManagerImpl;
-import io.github.retrooper.packetevents.sponge.InternalSpongeListener;
 import io.github.retrooper.packetevents.sponge.util.SpongeLogManager;
 import io.github.retrooper.packetevents.sponge.util.SpongeReflectionUtil;
+import io.github.retrooper.packetevents.sponge.util.viaversion.CustomPipelineUtil;
 import io.github.retrooper.packetevents.sponge.util.viaversion.ViaVersionUtil;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.scheduler.Task;
@@ -104,13 +100,10 @@ public class SpongePacketEventsBuilder {
                 try {
                     SpongeReflectionUtil.init();
                     CustomPipelineUtil.init();
-                    WrappedBlockState.ensureLoad();
-                    SynchronizedRegistriesHandler.init();
                 } catch (Exception ex) {
                     throw new IllegalStateException(ex);
                 }
-
-                PacketType.prepare();
+                super.load();
 
                 // Server hasn't bound to the port yet.
                 lateBind = !injector.isServerBound();
@@ -120,10 +113,6 @@ public class SpongePacketEventsBuilder {
                 }
 
                 loaded = true;
-
-                // Register internal packet listener (should be the first listener)
-                // This listener doesn't do any modifications to the packets, just reads data
-                getEventManager().registerListener(new InternalSpongePacketListener());
             }
 
             @Override
@@ -168,14 +157,10 @@ public class SpongePacketEventsBuilder {
             @Override
             public void terminate() {
                 if (initialized) {
-                    // Uninject the injector if needed(depends on the injector implementation)
-                    injector.uninject();
+                    super.terminate();
                     for (User user : this.protocolManager.getUsers()) {
                         ServerConnectionInitializer.destroyHandlers(user.getChannel());
                     }
-
-                    // Unregister all listeners. Because if we attempt to reload, we will end up with duplicate listeners.
-                    getEventManager().unregisterAllListeners();
                     initialized = false;
                     terminated = true;
                 }

commit 651d8bcfd9cd9ab7ad5a2f7f8f4392547a217e60

Fix velocity injection regression
Introduced by d0a97997181a860a4e7b7dbc4c92ac62295b7bf7

netty-common/src/main/java/io/github/retrooper/packetevents/impl/netty/factory/NettyPacketEventsBuilder.java
@@ -96,7 +96,7 @@ public class NettyPacketEventsBuilder {
                     PacketEvents.TIMEOUT_HANDLER_NAME = "pe-timeout-handler-" + id;
 
                     super.load();
-
+                    injector.inject();
                     loaded = true;
                 }
             }

velocity/src/main/java/io/github/retrooper/packetevents/velocity/factory/VelocityPacketEventsBuilder.java
@@ -176,7 +176,9 @@ public class VelocityPacketEventsBuilder {
                     PacketEvents.CONNECTION_HANDLER_NAME = "pe-connection-handler-" + id;
                     PacketEvents.SERVER_CHANNEL_HANDLER_NAME = "pe-connection-initializer-" + id;
                     PacketEvents.TIMEOUT_HANDLER_NAME = "pe-timeout-handler-" + id;
+
                     super.load();
+                    injector.inject();
                     loaded = true;
                 }
             }