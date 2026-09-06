b582f7184c5a6914fb13cdde04321ca45dde1826
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2026-09-06 18:30:40 +0530

    feat: dense table implementation for ids

--- api/src/main/java/com/github/retrooper/packetevents/util/mappings/TypesBuilder.java
+++ api/src/main/java/com/github/retrooper/packetevents/util/mappings/TypesBuilder.java
@@ -36,14 +36,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.Arrays;
+import java.util.function.IntConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @ApiStatus.Internal
 public class TypesBuilder {
     private final String mapPath;
-    private Map<ClientVersion, Map<String, Integer>> entries = new HashMap<>();
+    private @Nullable Map<String, int[]> idsByName = new HashMap<>();
+    private int[] fileSlotByVersionOrdinal = new int[0];
+    private int[] slotBySortedIndex = new int[0];
     private VersionMapper versionMapper;
 
     @Nullable
@@ -61,8 +64,8 @@ public class TypesBuilder {
     }
 
     public void load() {
-        if (this.entries == null) {
-            this.entries = new HashMap<>();
+        if (this.idsByName == null) {
+            this.idsByName = new HashMap<>();
         }
         try (final SequentialNBTReader.Compound rootCompound = MappingHelper.decompress("mappings/" + this.mapPath)) {
             rootCompound.skipOne(); // skip version tag for now
@@ -79,7 +82,13 @@ public class TypesBuilder {
                 loadAsMap(first, entries, versions);
             }
 
-            versionMapper = new VersionMapper(versions);
+            this.fileSlotByVersionOrdinal = new int[ClientVersion.values().length];
+            Arrays.fill(this.fileSlotByVersionOrdinal, -1);
+            for (int slot = 0; slot < versions.length; slot++) {
+                this.fileSlotByVersionOrdinal[versions[slot].ordinal()] = slot;
+            }
+            this.versionMapper = new VersionMapper(versions);
+            this.rebuildSlotMapping();
         } catch (IOException e) {
             throw new RuntimeException("Unable to load mapping files.", e);
         }
@@ -97,27 +106,46 @@ public class TypesBuilder {
             lastEntries.add(((NBTString) entry).getValue());
         }
 
-        final Consumer<ClientVersion> mapLoader = version -> {
-            final Map<String, Integer> map = new HashMap<>();
+        final int slots = versions.length;
+        final IntConsumer slotLoader = slot -> {
             int size = lastEntries.size();
             for (int i = 0; i < size; i++) {
-                map.put(lastEntries.get(i), i);
+                this.idsFor(lastEntries.get(i), slots)[slot] = i;
             }
-            this.entries.put(version, map);
         };
-        mapLoader.accept(start);
+        slotLoader.accept(0);
 
         int i = 1;
         for (Map.Entry<String, NBT> entry : entries) {
             final ClientVersion version = ClientVersion.valueOf(entry.getKey());
-            versions[i++] = version;
+            versions[i] = version;
             final List<ListDiff<String>> diff = MappingHelper.createListDiff((SequentialNBTReader.Compound) entry.getValue());
 
             for (int j = diff.size() - 1; j >= 0; j--) {
                 diff.get(j).applyTo(lastEntries);
             }
-            mapLoader.accept(version);
+            slotLoader.accept(i++);
         }
+    }
+
+    private int[] idsFor(String name, int slots) {
+        int[] ids = this.idsByName.get(name);
+        if (ids == null) {
+            ids = new int[slots];
+            Arrays.fill(ids, -1);
+            this.idsByName.put(name, ids);
+        }
+        return ids;
+    }
+
+    private void rebuildSlotMapping() {
+        ClientVersion[] sorted = this.versionMapper.getVersions();
+        int[] mapping = new int[sorted.length];
+        for (int i = 0; i < sorted.length; i++) {
+            int slot = this.fileSlotByVersionOrdinal[sorted[i].ordinal()];
+            mapping[i] = slot != -1 ? slot : (i > 0 ? mapping[i - 1] : 0);
+        }
+        this.slotBySortedIndex = mapping;
     }
 
     private void loadAsMap(
@@ -130,22 +158,24 @@ public class TypesBuilder {
         final Map<String, Integer> lastEntries = StreamSupport.stream(((SequentialNBTReader.Compound) first.getValue()).spliterator(), false)
                 .collect(Collectors.toMap(Map.Entry::getKey, entry -> ((NBTNumber) entry.getValue()).getAsInt()));
 
-        final Consumer<ClientVersion> mapLoader = version -> {
-            final Map<String, Integer> map = new HashMap<>(lastEntries);
-            this.entries.put(version, map);
+        final int slots = versions.length;
+        final IntConsumer slotLoader = slot -> {
+            for (Map.Entry<String, Integer> e : lastEntries.entrySet()) {
+                this.idsFor(e.getKey(), slots)[slot] = e.getValue();
+            }
         };
-        mapLoader.accept(start);
+        slotLoader.accept(0);
 
         int i = 1;
         for (Map.Entry<String, NBT> entry : entries) {
             final ClientVersion version = ClientVersion.valueOf(entry.getKey());
-            versions[i++] = version;
+            versions[i] = version;
             final List<MapDiff<String, Integer>> diff = MappingHelper.createDiff((SequentialNBTReader.Compound) entry.getValue());
 
             for (MapDiff<String, Integer> d : diff) {
                 d.applyTo(lastEntries);
             }
-            mapLoader.accept(version);
+            slotLoader.accept(i++);
         }
     }
 
@@ -172,42 +202,55 @@ public class TypesBuilder {
     public void addExtraVersionStep(ClientVersion version) {
         VersionMapper newMapper = this.versionMapper.withExtra(version);
         if (this.versionMapper != newMapper) {
-            // version was inserted, update entries
-            int baseIndex = this.versionMapper.getIndex(version);
-            ClientVersion baseVersion = this.versionMapper.getVersions()[baseIndex];
-            this.entries.put(version, this.entries.get(baseVersion));
             this.versionMapper = newMapper; // save new mapper
+            this.rebuildSlotMapping(); // the reads the nearest older version's slot
         }
     }
 
     @VisibleForTesting
     public boolean isMappingDataLoaded() {
-        return this.entries != null;
+        return this.idsByName != null;
     }
 
     public void unloadFileMappings() {
-        entries.clear();
-        entries = null;
+        if (this.idsByName != null) {
+            this.idsByName.clear();
+            this.idsByName = null;
+        }
     }
 
     public TypesBuilderData define(String key, VersionRange range) {
         final ResourceLocation name = new ResourceLocation(key);
-        final int[] ids = new int[this.getVersions().length];
-        int index = 0;
-        for (ClientVersion v : this.getVersions()) {
-            final Map<String, Integer> map = entries.get(v);
-            if (map.containsKey(key)) {
-                int id = map.get(key);
-                ids[index] = id;
-            } else {
-                ids[index] = -1;
+        final int[] slots = this.slotBySortedIndex;
+        final int[] ids = new int[slots.length];
+        final int[] fileIds = this.idsByName.get(key);
+        if (fileIds == null) {
+            Arrays.fill(ids, -1);
+        } else {
+            for (int i = 0; i < slots.length; i++) {
+                ids[i] = fileIds[slots[i]];
             }
-            index++;
         }
         return new TypesBuilderData(name, ids, this, range);
     }
 
     public @Nullable Map<ClientVersion, Map<String, Integer>> getEntries() {
-        return this.entries;
+        if (this.idsByName == null) {
+            return null;
+        }
+        ClientVersion[] sorted = this.versionMapper.getVersions();
+        Map<ClientVersion, Map<String, Integer>> view = new HashMap<>(sorted.length * 2);
+        for (int i = 0; i < sorted.length; i++) {
+            Map<String, Integer> perVersion = new HashMap<>();
+            int slot = this.slotBySortedIndex[i];
+            for (Map.Entry<String, int[]> entry : this.idsByName.entrySet()) {
+                int id = entry.getValue()[slot];
+                if (id != -1) {
+                    perVersion.put(entry.getKey(), id);
+                }
+            }
+            view.put(sorted[i], perVersion);
+        }
+        return view;
     }
 }
